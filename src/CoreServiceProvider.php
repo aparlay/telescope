c45d6085def07079fbd2f3bc496b6421b47724a4
Ramin Farmani <ramin.farmani@gmail.com>
2021-07-17 23:55:38 +0430

    Init core package

---
+++ src/CoreServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Aparlay\Core;
+
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class CoreServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->configureRateLimiting();
+
+        include __DIR__ . '/routes/api.php';
+        include __DIR__ . '/routes/web.php';
+        include __DIR__ . '/routes/admin.php';
+        include __DIR__ . '/routes/channels.php';
+        include __DIR__ . '/routes/console.php';
+    }
+
+
+    /**
+     * Configure the rate limiters for the application.
+     *
+     * @return void
+     */
+    protected function configureRateLimiting()
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+        });
+    }
+}
---
+++ src/Models/User.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        '_id',
+        'username',
+        'email',
+        'email_verified',
+        'phone_number',
+        'phone_number_verified',
+        'bio',
+        'full_name',
+        'avatar',
+        'auth_key',
+        'password_hash',
+        'password_reset_token',
+        'setting',
+        'features',
+        'gender',
+        'interested_in',
+        'type',
+        'status',
+        'visibility',
+        'follower_count',
+        'following_count',
+        'block_count',
+        'followed_hashtag_count',
+        'like_count',
+        'media_count',
+        'count_fields_updated_at',
+        'blocks',
+        'likes',
+        'followers',
+        'followings',
+        'followed_hashtags',
+        'medias',
+        'promo_link',
+        'referral_id',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        '_id' => 'string',
+        'email_verified_at' => 'datetime',
+        'phone_number_verified_at' => 'datetime',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime',
+    ];
+}
