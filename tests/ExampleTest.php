ca85f6064ea941d2af7e43acb5dba82f4565c544
Ramin Farmani <ramin.farmani@gmail.com>
2021-07-18 10:45:30 +0430

    Init core package

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Aparlay\Core\Tests;
+
+
+class ExampleTest extends \PHPUnit\Framework\TestCase
+{
+
+    /** @test */
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
