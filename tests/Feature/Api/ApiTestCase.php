34b9ec29d86a694eb2181f4746fee0753418c942
farmani <farmani@users.noreply.github.com>
2021-07-20 12:59:02 +0000

    Fix styling

--- tests/Feature/Api/ApiTestCase.php
+++ tests/Feature/Api/ApiTestCase.php
@@ -2,8 +2,6 @@
 
 namespace Aparlay\Core\Tests\Feature\Api;
 
-use Aparlay\Core\CoreServiceProvider;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Aparlay\Core\Tests\TestCase;
 
 class ApiTestCase extends TestCase
