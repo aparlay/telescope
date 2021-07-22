3b32b3434285f2afdab7a59a5ab23ce56cc46d13
Prashant <prashant.ratanparikhe@gmail.com>
2021-07-22 18:46:14 +0530

    Auth: Login and Signup

--- src/Api/V1/Controllers/AuthController.php
+++ src/Api/V1/Controllers/AuthController.php
@@ -6,6 +6,9 @@ use Aparlay\Core\Api\V1\Models\Block;
 use Illuminate\Http\Request;
 
 use Illuminate\Http\Response;
+use Validator;
+use Aparlay\Core\Api\V1\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -16,7 +19,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('auth:api', ['except' => ['login','register']]);
     }
 
     /**
@@ -70,14 +73,58 @@ class AuthController extends Controller
      * @return Response
      */
     public function login(Request $request)
-    {
-        $credentials = request(['email', 'password']);
+    { 
+        $validator = Validator::make($request->all(), 
+                      [ 
+                      'email' => 'required|email',
+                      'password' => 'required'
+                     ]);  
+ 
+         if ($validator->fails()) {  
+            return response()->json(['error'=>$validator->errors()], 401); 
+        }
 
-        if (! $token = auth()->attempt($credentials)) {
+        if (! $token = auth()->attempt($validator->validated())) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
         return $this->response($this->respondWithToken($token));
+    }
+
+    /**
+     * Register a User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), 
+                      [ 
+                      'email' => 'required|email|unique:users|max:100',
+                      'password' => 'required|min:8|max:20',
+                      'gender' => 'required|numeric',
+                      'username' => 'nullable|min:6|max:20',
+                      'phone_number' => 'nullable|numeric',
+                     ]);  
+ 
+         if ($validator->fails()) {  
+            return response()->json(['error'=>$validator->errors()], 401); 
+        }   
+ 
+        $user = new User();
+        $user->email = $request->email;
+        $user->password_hash = Hash::make($request->password);
+        $user->username = ($request->username) ? $request->username : null;
+        $user->phone_number = ($request->phone_number) ? $request->phone_number : null;
+        $user->gender = $request->gender;
+        $user->status = User::STATUS_PENDING;
+        $user->visibility = User::VISIBILITY_PUBLIC;
+        $user->save();
+  
+        return response()->json([
+            'success' => true,
+            'data' => $user
+        ], Response::HTTP_OK);
     }
 
     /**
