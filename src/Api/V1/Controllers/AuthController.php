9010b78981156edcaabcf1ae2fba347405cb434b
Ramin Farmani <ramin.farmani@gmail.com>
2021-07-22 18:12:16 +0430

    Fix docker image

--- src/Api/V1/Controllers/AuthController.php
+++ src/Api/V1/Controllers/AuthController.php
@@ -4,10 +4,11 @@ namespace Aparlay\Core\Api\V1\Controllers;
 
 use Aparlay\Core\Api\V1\Models\Block;
 use Aparlay\Core\Api\V1\Models\User;
-
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 use Validator;
 
 class AuthController extends Controller
@@ -19,16 +20,15 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login','register']]);
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  Block  $media
-     * @return Response
+     * @return JsonResponse
      */
-    public function token(Block $media)
+    public function token()
     {
         //
     }
@@ -36,10 +36,9 @@ class AuthController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  Block  $media
-     * @return Response
+     * @return JsonResponse
      */
-    public function changePassword(Block $media)
+    public function changePassword()
     {
         //
     }
@@ -47,10 +46,9 @@ class AuthController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  Block  $media
-     * @return Response
+     * @return JsonResponse
      */
-    public function validateOtp(Block $media)
+    public function validateOtp()
     {
         //
     }
@@ -58,10 +56,9 @@ class AuthController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  Block  $media
-     * @return Response
+     * @return JsonResponse
      */
-    public function requestOtp(Block $media)
+    public function requestOtp()
     {
         //
     }
@@ -70,104 +67,112 @@ class AuthController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  Request  $request
-     * @return Response
+     * @return JsonResponse
+     * @throws ValidationException
      */
     public function login(Request $request)
     {
         $validator = Validator::make(
             $request->all(),
             [
-                      'email' => 'required|email',
-                      'password' => 'required',
-                     ]
+                'email' => 'required|email',
+                'password' => 'required',
+            ]
         );
 
         if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors()], 401);
+            return $this->error(
+                __('Data Validation Failed'),
+                $validator->errors()->toArray(),
+                Response::HTTP_UNPROCESSABLE_ENTITY
+            );
         }
 
-        if (! $token = auth()->attempt($validator->validated())) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+        if (!$token = auth()->attempt($validator->validated())) {
+            return $this->error(
+                __('Data Validation Failed'),
+                $validator->errors()->toArray(),
+                Response::HTTP_UNAUTHORIZED
+            );
         }
 
         return $this->response($this->respondWithToken($token));
     }
 
     /**
-     * Register a User.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function register(Request $request)
-    {
-        $validator = Validator::make(
-            $request->all(),
-            [
-                      'email' => 'required|email|unique:users|max:100',
-                      'password' => 'required|min:8|max:20',
-                      'gender' => 'required|numeric',
-                      'username' => 'nullable|min:6|max:20',
-                      'phone_number' => 'nullable|numeric',
-                     ]
-        );
-
-        if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors()], 401);
-        }
-
-        $user = new User();
-        $user->email = $request->email;
-        $user->password_hash = Hash::make($request->password);
-        $user->username = ($request->username) ? $request->username : null;
-        $user->phone_number = ($request->phone_number) ? $request->phone_number : null;
-        $user->gender = $request->gender;
-        $user->status = User::STATUS_PENDING;
-        $user->visibility = User::VISIBILITY_PUBLIC;
-        $user->save();
-
-        return response()->json([
-            'success' => true,
-            'data' => $user,
-        ], Response::HTTP_OK);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  Block  $media
-     * @return Response
-     */
-    public function logout(Block $media)
-    {
-        auth()->logout();
-
-        return $this->response(['message' => 'Success']);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  Block  $media
-     * @return Response
-     */
-    public function refresh(Block $media)
-    {
-        return $this->respondWithToken(auth()->refresh());
-    }
-
-    /**
      * Get the token array structure.
      *
-     * @param  string $token
+     * @param  string  $token
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @return array
      */
-    protected function respondWithToken($token)
+    protected function respondWithToken(string $token): array
     {
         return [
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60,
         ];
+    }
+
+    /**
+     * Register a User.
+     *
+     * @return JsonResponse
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'email' => 'required|email|unique:users|max:100',
+                'password' => 'required|min:8|max:20',
+                'gender' => 'required|numeric',
+                'username' => 'nullable|min:6|max:20',
+                'phone_number' => 'nullable|numeric',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return $this->error(
+                __('Data Validation Failed'),
+                $validator->errors()->toArray(),
+                Response::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
+
+        $user = new User();
+        $user->email = $request->email;
+        $user->password_hash = Hash::make($request->password);
+        $user->username = ($request->username) ?: null;
+        $user->phone_number = ($request->phone_number) ?: null;
+        $user->gender = $request->gender;
+        $user->status = User::STATUS_PENDING;
+        $user->visibility = User::VISIBILITY_PUBLIC;
+        $user->save();
+
+        return $this->response(['success' => true, 'data' => $user], '', Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @return JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return $this->response([], '', Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @return JsonResponse
+     */
+    public function refresh(): JsonResponse
+    {
+        return $this->response($this->respondWithToken(auth()->refresh()));
     }
 }
