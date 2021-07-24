8525ba39e640bf58f5784d9d8e6c3c50a8294445
Prashant <prashant.ratanparikhe@gmail.com>
2021-07-24 17:12:15 +0530

    Set Default Avatar

--- src/Api/V1/Requests/UserRequest.php
+++ src/Api/V1/Requests/UserRequest.php
@@ -2,6 +2,7 @@
 
 namespace Aparlay\Core\Api\V1\Requests;
 
+use Aparlay\Core\Api\V1\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UserRequest extends FormRequest
@@ -26,8 +27,14 @@ class UserRequest extends FormRequest
         return [];
     }
 
+    /**
+     * This function is responsible to perform pre-validation tasks like
+     * Set the email or phone, based on username
+     * Set the avatar, based on gender
+     */
     public function prepareForValidation()
     {
+        /** Set email or phone basd on the usernmae format */
         if (strpos($this->username, '@') !== false) {
             $this->email = $this->username;
         }
@@ -36,10 +43,27 @@ class UserRequest extends FormRequest
         }
         $this->username = uniqid('', false);
 
+        /** Set avatar based on Gender */
+        if (empty($this->avatar)) {
+            switch ($this->gender) {
+                case User::GENDER_FEMALE:
+                    $filename = 'default_fm_' . random_int(1, 60) . '.png';
+                    break;
+                case User::GENDER_MALE:
+                    $filename = 'default_m_' . random_int(1,120)  . '.png';
+                    break;
+                default:
+                    $filename = (((bool)random_int(0, 1)) ? 'default_m_' . random_int(1,120) : 'default_fm_' . random_int(1, 60)) . '.png';
+            }
+            $this->avatar = config('app.cdn.avatars') . $filename;
+        }
+
+        /** Set the request parameters implemented above */
         $this->merge([
             'username' => $this->username,
             'email' => $this->email,
             'phone_number' => $this->phone_number,
+            'avatar' => $this->avatar,
         ]);
     }
 }
