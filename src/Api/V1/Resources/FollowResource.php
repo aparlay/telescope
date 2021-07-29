23311257b4b2bb0d2db66bacc0c4dffb9358348a
Ramin Farmani <ramin.farmani@gmail.com>
2021-07-29 09:57:10 +0430

    Adding Resources
    Add resources for the response

---
+++ src/Api/V1/Resources/FollowResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Aparlay\Core\Api\V1\Resources;
+
+use Aparlay\Core\Helpers\DT;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FollowResource extends JsonResource
+{
+    use SimpleUserTrait;
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            '_id' => (string)$this->_id,
+            'creator' => $this->createSimpleUser($this->creator, ['_id', 'username', 'avatar', 'is_followed']),
+            'user' => $this->createSimpleUser($this->user, ['_id', 'username', 'avatar', 'is_followed']),
+            'created_at' => $this->created_at->valueOf(),
+        ];
+    }
+}
---
+++ src/Api/V1/Resources/ReportCollection.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Aparlay\Core\Api\V1\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ReportCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
