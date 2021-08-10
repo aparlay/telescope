19ae2a28fb1f5671943b4f54ad9c11350f6b1ea3
alizoj <kzt1@bk.ru>
2021-08-10 07:09:26 -0700

    Delete use repository service

--- config/core.php
+++ config/core.php
@@ -3,7 +3,6 @@
 // config for Aparlay/Core/ClassName
 use Aparlay\Core\Api\V1\Providers\AuthServiceProvider;
 use Aparlay\Core\Api\V1\Providers\EventServiceProvider;
-use Aparlay\Core\Providers\RepositoryServiceProvider;
 
 return [
 
