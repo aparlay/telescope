3bc07776111e4dbf0453671d6da6d8236d6eae93
Ramin Farmani <ramin.farmani@gmail.com>
2021-08-13 09:15:15 +0430

    Fix user repository

--- src/Repositories/UserRepository.php
+++ src/Repositories/UserRepository.php
@@ -2,12 +2,23 @@
 
 namespace Aparlay\Core\Repositories;
 
-use App\Models\User;
-use Illuminate\Contracts\Auth\Authenticatable;
+use Aparlay\Core\Models\User;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Contracts\Auth\Authenticatable;
 
-class UserRepository
+class UserRepository implements RepositoryInterface
 {
+    protected User $model;
+
+    public function __construct(User $model)
+    {
+        if (!($model instanceof User)) {
+            throw new \InvalidArgumentException('$model should be of User type');
+        }
+
+        $this->model = $model;
+    }
+
     public function verify(User $user)
     {
         $user->status = User::STATUS_VERIFIED;
@@ -18,13 +29,13 @@ class UserRepository
     /**
      * Through exception if user is suspended/banned/not found.
      *
-     * @param User|Authenticatable $user
+     * @param  User|Authenticatable  $user
      *
      * @return bool
      *
      * @throws ValidationException
      */
-    public function isUserEligible(User | Authenticatable $user)
+    public function isUserEligible(User|Authenticatable $user)
     {
         switch ($user->status) {
             case User::STATUS_SUSPENDED:
@@ -48,12 +59,37 @@ class UserRepository
 
     /**
      * Responsible to check if OTP is required to sent to the user, based on user_status and otp settings.
-     * @param User $user
+     * @param  User  $user
      * @return bool
      */
     public function isUnverified(User $user)
     {
         /* User is considered as unverified when "OTP Setting is enabled AND user status is pending" */
         return $user->setting['otp'] && $user->status === User::STATUS_PENDING;
+    }
+
+    public function all()
+    {
+        // TODO: Implement all() method.
+    }
+
+    public function create(array $data)
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function update(array $data, $id)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public function find($id)
+    {
+        // TODO: Implement find() method.
     }
 }
