ae646893569741b8292e63749f3839862847d1a8
alizoj <kzt1@bk.ru>
2021-08-17 05:41:07 -0700

    Fix PR issues

--- src/Api/V1/Controllers/MediaController.php
+++ src/Api/V1/Controllers/MediaController.php
@@ -7,7 +7,7 @@ use Aparlay\Core\Api\V1\Models\User;
 use Aparlay\Core\Api\V1\Requests\MediaRequest;
 use Aparlay\Core\Api\V1\Resources\MediaResource;
 use Aparlay\Core\Repositories\MediaRepository;
-use Aparlay\Core\Services\BackBlaze;
+use Aparlay\Core\Services\UploadService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -47,13 +47,11 @@ class MediaController extends Controller
         return $this->repository->store($request);
     }
 
-    public function uploadToken()
+    public function upload(): Response
     {
-        $b2 = new BackBlaze();
-        $result = $b2->generateToken();
-        $result['filename'] = uniqid(auth()->user()->_id.'_', false);
+        $result = UploadService::chunkUpload();
 
-        return $result;
+        return $this->response($result['data'], '', $result['code']);
     }
 
     /**
