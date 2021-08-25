d13716b663313bc1449be7810ffe9c93ee389ee4
alizoj <kzt1@bk.ru>
2021-08-25 09:42:05 -0700

    Remove unused method from BaseBuilder

--- src/Builders/BaseBuilder.php
+++ src/Builders/BaseBuilder.php
@@ -9,30 +9,6 @@ use Illuminate\Database\Eloquent\Builder;
 class BaseBuilder extends Builder
 {
     /**
-     * Create a new paginator instance.
-     *
-     * @param \Illuminate\Support\Collection $items
-     * @param int $total
-     * @param int $perPage
-     * @param int $currentPage
-     * @param array $options
-     * @return CorePaginator
-     */
-//    protected function paginator($items, $total, $perPage, $currentPage, $options)
-//    {
-//        return Container::getInstance()->makeWith(
-//            CorePaginator::class,
-//            compact(
-//                'items',
-//                'total',
-//                'perPage',
-//                'currentPage',
-//                'options'
-//            )
-//        );
-//    }
-
-    /**
      * Paginate the given query.
      *
      * @param int|null $perPage
