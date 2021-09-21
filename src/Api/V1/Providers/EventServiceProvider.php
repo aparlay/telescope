c3c5d2f9ea87e980158f98cee85acd03685aa2ce
jayson-suizo <jayson-suizo@users.noreply.github.com>
2021-09-21 10:00:42 +0000

    Fix styling

--- src/Api/V1/Providers/EventServiceProvider.php
+++ src/Api/V1/Providers/EventServiceProvider.php
@@ -6,16 +6,15 @@ use Aparlay\Core\Api\V1\Models\Block;
 use Aparlay\Core\Api\V1\Models\Media;
 use Aparlay\Core\Api\V1\Models\MediaLike;
 use Aparlay\Core\Api\V1\Models\MediaVisit;
-use Aparlay\Core\Api\V1\Models\User;
 use Aparlay\Core\Api\V1\Models\Report;
+use Aparlay\Core\Api\V1\Models\User;
 use Aparlay\Core\Models\Model;
 use Aparlay\Core\Observers\BlockObserver;
 use Aparlay\Core\Observers\MediaLikeObserver;
 use Aparlay\Core\Observers\MediaObserver;
 use Aparlay\Core\Observers\MediaVisitObserver;
-use Aparlay\Core\Observers\UserObserver;
 use Aparlay\Core\Observers\ReportObserver;
-
+use Aparlay\Core\Observers\UserObserver;
 
 class EventServiceProvider extends \Aparlay\Core\Providers\EventServiceProvider
 {
--- src/Observers/ReportObserver.php
+++ src/Observers/ReportObserver.php
@@ -2,25 +2,22 @@
 
 namespace Aparlay\Core\Observers;
 
+use Aparlay\Core\Api\V1\Notifications\ReportSent;
 use Aparlay\Core\Helpers\DT;
 use Aparlay\Core\Models\Report;
-use Aparlay\Core\Api\V1\Notifications\ReportSent;
-
 use MongoDB\BSON\ObjectId;
 
 class ReportObserver
 {
-     /**
+    /**
      * Handle the Report "creating" event.
      *
      * @param  Report  $report
      * @return void
      */
-    public function creating(Report  $report)
+    public function creating(Report $report)
     {
-        
     }
-
 
     /**
      * Handle the Report "created" event.
@@ -29,8 +26,7 @@ class ReportObserver
      * @return void
      */
     public function created(Report $report)
-    {  
-       $report->notify( new ReportSent());
+    {
+        $report->notify(new ReportSent());
     }
-
 }
