39b5f90a0201370042c12cd42a8884a6032be8b3
Ramin Farmani <ramin.farmani@gmail.com>
2021-10-05 10:30:35 +0330

    Fix upload video

--- src/Api/V1/Services/UploadService.php
+++ src/Api/V1/Services/UploadService.php
@@ -4,7 +4,6 @@ namespace Aparlay\Core\Api\V1\Services;
 
 use Flow\Config;
 use Flow\File;
-use Flow\Request as FlowRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -12,27 +11,27 @@ class UploadService
 {
     public static function chunkUpload(Request $request): array
     {
-        $config = new Config();
-        $config->setTempDir(sys_get_temp_dir());
-        $code = 500;
+        $config = new Config(array(
+            'tempDir' => '/var/www/aparlay/alua/storage/app/chunk/', //With write access
+        ));
 
-        $result = ['data' => [], 'code' => $code];
-
-        $flowRequest = new FlowRequest($request->all(), $request->file('file'));
-        $fileName = strtolower($flowRequest->getFileName());
-        $fileName = uniqid('tmp_', true).'.'.pathinfo($fileName, PATHINFO_EXTENSION);
+        $result = ['data' => [], 'code' => 500];
 
         $file = new File($config);
         if ($request->isMethod('GET')) {
             $result['code'] = $file->checkChunk() ? 200 : 204;
 
             return $result;
-        } elseif ($file->validateChunk()) {
+        }
+
+        if ($file->validateChunk()) {
             $file->saveChunk();
         } else {
             abort(400, __('Invalid chunk uploaded'));
         }
 
+        $fileName = strtolower($request->input('flowFilename'));
+        $fileName = uniqid('tmp_', true).'.'.pathinfo($fileName, PATHINFO_EXTENSION);
         if ($file->validateFile() && $file->save('/var/www/aparlay/alua/storage/app/upload/'.$fileName)) {
             $result['data'] = ['file' => $fileName];
             $result['code'] = 201;
