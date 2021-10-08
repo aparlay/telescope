767f735ff062ce1bdae6519a63f5bfbd95ddd405
Ramin Farmani <ramin.farmani@gmail.com>
2021-10-08 08:54:02 +0330

    Remove cache

--- routes/api.php
+++ routes/api.php
@@ -41,7 +41,7 @@ Route::middleware(['api', 'format-response', 'device-id', 'device-id-trottle'])-
         });
 
         /* Cache Group */
-        Route::middleware(['cache.headers:no-transform', 'auth:api', 'cookies-auth'])->group(function () {
+        Route::middleware(['cache.headers:no_transform', 'auth:api', 'cookies-auth'])->group(function () {
             Route::get('/{media}', [MediaController::class, 'show'])->name('show')->withoutMiddleware(['auth:api']);
         });
     });
