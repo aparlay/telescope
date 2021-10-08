b7d32fd2452d65ed0fa2aebbe51f2d10fa6ed132
Ramin Farmani <ramin.farmani@gmail.com>
2021-10-08 09:56:24 +0330

    Remove cache

--- src/Api/V1/Http/Kernel.php
+++ src/Api/V1/Http/Kernel.php
@@ -17,6 +17,7 @@ class Kernel extends HttpKernel
         'device-id' => \Aparlay\Core\Api\V1\Http\Middleware\DeviceId::class,
         'cookies-auth' => \Aparlay\Core\Api\V1\Http\Middleware\CookiesAuthenticate::class,
         'device-id-trottle' => \Aparlay\Core\Api\V1\Http\Middleware\DeviceIdTrottle::class,
+        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
     ];
 
     /**
