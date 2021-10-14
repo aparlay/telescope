8528c8484875ab7e7420da5402f50ac19fa8ba8a
Ramin Farmani <ramin.farmani@gmail.com>
2021-10-14 09:39:52 +0330

    Fix swagger documents

--- src/Api/V1/Models/Block.php
+++ src/Api/V1/Models/Block.php
@@ -9,7 +9,7 @@ use Aparlay\Core\Models\Block as BlockBase;
  */
 class Block extends BlockBase
 {
-    /*
+    /**
      * @OA\Property(property="_id", type="string", example="60237caf5e41025e1e3c80b1")
      * @OA\Property(property="created_at", type="string", example="1612850111566")
      * @OA\Property(property="creator", ref="#/components/schemas/SimpleUser")
