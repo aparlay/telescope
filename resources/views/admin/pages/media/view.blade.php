7b7f90da3f1c610cb88fc48974ddb588576b863e
Prashant <prashant.ratanparikhe@gmail.com>
2021-10-20 19:45:15 +0530

    Saprate JS file

--- resources/views/admin/pages/media/view.blade.php
+++ resources/views/admin/pages/media/view.blade.php
@@ -329,25 +329,6 @@
 @endsection
 
 @section('scripts')
-@section('js')
-<script>
-    
-    $('.skin_score_lable').click(function(){
-        $('.skin_score_lable').removeClass('active');
-        $(this).addClass('active');
-    });
-    
-    $('.awesomeness_score_label').click(function(){
-        $('.awesomeness_score_label').removeClass('active');
-        $(this).addClass('active');
-    });
-
-    $(window).load(function(){
-        setTimeout(function(){
-            $('.alert').hide();
-        }, 500);
-    });
-    
-</script>
+<script src="{{ asset('admin/assets/js/adminMedia.js') }}"></script>
 @endsection
 
