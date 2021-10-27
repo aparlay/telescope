34158d2e4909c0cee86eb0d968684b6cb1409250
Prashant <prashant.ratanparikhe@gmail.com>
2021-10-27 17:23:59 +0530

    resolved comments

--- resources/views/admin/layouts/media_view_header.blade.php
+++ resources/views/admin/layouts/media_view_header.blade.php
@@ -39,18 +39,4 @@
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
     </div>
-    <div id="app">
-        @if ($message = Session::get('success'))
-            <div class="alert alert-success alert-block">
-                <button type="button" class="close" data-dismiss="alert">×</button>	
-                <i class="icon fas fa-check"></i><strong>{{ $message }}</strong>
-            </div>
-        @endif
-        @if ($message = Session::get('danger'))
-            <div class="alert alert-danger alert-block">
-                <button type="button" class="close" data-dismiss="alert">×</button>	
-                    <strong>{{ $message }}</strong>
-            </div>
-        @endif
-    </div>
     <!-- /.content-header -->
