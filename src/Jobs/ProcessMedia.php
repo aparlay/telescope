3aa794b68d89a118a8dcdd8ae91b21bc99df37ec
farmani <farmani@users.noreply.github.com>
2021-11-02 09:25:54 +0000

    Fix styling

--- src/Jobs/ProcessMedia.php
+++ src/Jobs/ProcessMedia.php
@@ -133,7 +133,7 @@ class ProcessMedia implements ShouldQueue
             $media->status = Media::STATUS_FAILED;
             $media->save($withoutTouch);
             Log::error(__CLASS__.PHP_EOL.'Cannot check video duration');
-            //throw new Exception(__CLASS__.PHP_EOL.'Cannot check video duration');
+        //throw new Exception(__CLASS__.PHP_EOL.'Cannot check video duration');
         } else {
             $media->length = (float) $response->GetSec();
             $media->addToSet('processing_log', '3. Duration checked: '.$media->length.' Sec');
