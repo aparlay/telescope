d143abdb0c5091cb24b36c30fe4132617e1a6037
jayson-suizo <jayson.b.suizo@gmail.com>
2021-11-03 20:01:35 +0800

    update

--- tests/Browser/Admin/LoginTest.php
+++ tests/Browser/Admin/LoginTest.php
@@ -4,17 +4,18 @@ namespace Aparlay\Core\Tests\Browser\Admin;
 
 use Aparlay\Core\Admin\Models\User;
 use Laravel\Dusk\Browser;
-use MongoDB\BSON\ObjectId;
 use Tests\DuskTestCase;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
 
 class LoginTest extends DuskTestCase
 {
     /**
-     * A test if admin login page is working.
+     * A test if admin login page is working.NN
      *
      * @test
      * @return void
-     * @throws \Throwable
+     * @throws \ThrowableN
      */
     public function visitAdmin()
     {
@@ -34,13 +35,25 @@ class LoginTest extends DuskTestCase
      */
     public function loginAdmin()
     {
-        $active_admin = User::role('super-administrator')->first();
-        $this->browse(function ($browser) use ($active_admin) {
+        $super_admin = User::factory()->create([
+            'email' => uniqid('alua_').'@aparly.com',
+            'status' => User::STATUS_ACTIVE,
+            'type' => 1,
+            'password_hash' => Hash::make('password'),
+            'email_verified' => true,
+        ]);
+        $super_admin = User::find($super_admin->_id);
+
+        $super_admin->assignRole('super-administrator');
+
+        $this->browse(function ($browser) use ($super_admin) {
             $browser->visit('/login')
-                    ->type('email', $active_admin->email)
+                    ->type('email', $super_admin->email)
                     ->type('password', 'password')
                     ->press('Sign In')
                     ->assertPathIs('/dashboard');
         });
+
+
     }
 }
