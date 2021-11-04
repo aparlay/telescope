1eab507d1d7a4f14859094936bcac2d70a982621
Ramin Farmani <ramin.farmani@gmail.com>
2021-11-04 17:16:36 +0330

    Resolve like issue

--- src/Models/MediaLike.php
+++ src/Models/MediaLike.php
@@ -126,9 +126,9 @@ class MediaLike extends BaseModel
         $cacheKey = (new self())->getCollection().':creator:'.$userId;
 
         if (! Redis::exists($cacheKey)) {
-            $likedMediaIds = self::project(['media_ids' => true, '_id' => false])
+            $likedMediaIds = self::project(['media_id' => true, '_id' => false])
                 ->creator($userId)
-                ->pluck('media_ids')
+                ->pluck('media_id')
                 ->toArray();
 
             if (empty($likedMediaIds)) {
