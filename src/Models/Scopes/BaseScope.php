e354107a270bdea89d106eba6a4b8a7299b6d2f4
Franz <franzbrianbriones@gmail.com>
2021-11-17 20:06:56 +0800

    update from PR 139

--- src/Models/Scopes/BaseScope.php
+++ src/Models/Scopes/BaseScope.php
@@ -56,21 +56,4 @@ trait BaseScope
 
         return $query;
     }
-
-    public function scopeDate(Builder $query, UTCDateTime $start = null, UTCDateTime $end = null): Builder
-    {
-        if (null !== $start && null !== $end) {
-            return $query->whereBetween('created_at', [$start, $end]);
-        }
-
-        if (null !== $start) {
-            return $query->where('created_at', '$gte', $start);
-        }
-
-        if (null !== $end) {
-            return $query->where('created_at', '$lte', $end);
-        }
-
-        return $query;
-    }
 }
