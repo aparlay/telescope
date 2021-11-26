a01578f948a9bc50ae2c7f1c2eecf485ecf4aa9e
jayson-suizo <jayson.b.suizo@gmail.com>
2021-11-26 17:54:56 +0800

    add default value for tips inside medias

---
+++ database/migrations/2021_11_26_092609_add_default_value_for_tip_in_media.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Media;
+
+class AddDefaultValueForTipInMedia extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Media::where('tips', null)->chunk(200, function ($models) {
+            foreach ($models as $media) {
+                $media->tips = 0;
+                $media->save();
+            }
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
