40bb82b6528b6c5790f9701843d4ca47adacd592
Ramin Farmani <ramin.farmani@gmail.com>
2021-12-02 13:31:47 +0330

    fix email scopes

--- src/Models/Scopes/EmailScope.php
+++ src/Models/Scopes/EmailScope.php
@@ -2,7 +2,38 @@
 
 namespace Aparlay\Core\Models\Scopes;
 
+use Aparlay\Core\Models\Email;
+use Aparlay\Core\Models\Media;
+use Illuminate\Database\Eloquent\Builder;
+
 trait EmailScope
 {
     use BaseScope;
+
+    /**
+     * @param  Builder  $query
+     * @return Builder
+     */
+    public function scopeOpened(Builder $query): Builder
+    {
+        return $query->where('status', Email::STATUS_OPENED);
+    }
+
+    /**
+     * @param  Builder  $query
+     * @return Builder
+     */
+    public function scopeSent(Builder $query): Builder
+    {
+        return $query->where('status', Email::STATUS_SENT);
+    }
+
+    /**
+     * @param  Builder  $query
+     * @return Builder
+     */
+    public function scopeFailed(Builder $query): Builder
+    {
+        return $query->where('status', Email::STATUS_FAILED);
+    }
 }
