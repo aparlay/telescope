ee5cf7d9a4f7e1afe88cee26f40208e2d4e482d2
jayson-suizo <jayson.b.suizo@gmail.com>
2021-12-14 18:12:53 +0800

    add email nofitications

--- src/Api/V1/Controllers/ContactUsController.php
+++ src/Api/V1/Controllers/ContactUsController.php
@@ -3,6 +3,7 @@
 namespace Aparlay\Core\Api\V1\Controllers;
 
 use Aparlay\Core\Api\V1\Models\Alert;
+use Aparlay\Core\Jobs\Email;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Aparlay\Core\Api\V1\Requests\ContactUsRequest;
@@ -17,6 +18,9 @@ class ContactUsController extends Controller
      */
     public function send( ContactUsRequest $request): Response
     {
+        $data = $request->input();
+        $data['msg'] =  $data['message'];
+        dispatch((new Email(config('mail.support_email'), 'Contact Us notification', 'email_contactus',$data))->onQueue('medium'));
         return $this->response([], Response::HTTP_OK);
     }
 
