2f7ea1e9356126c78ba6d8f35ca7c92fdd5be007
andy bogdanov <jehkinen@ya.ru>
2021-12-21 10:35:56 +0500

    Updated test to deactivate user

--- tests/Feature/Api/UserTest.php
+++ tests/Feature/Api/UserTest.php
@@ -3,11 +3,9 @@
 namespace Aparlay\Core\Tests\Feature\Api;
 
 use Aparlay\Core\Api\V1\Controllers\UserController;
-use Aparlay\Core\Api\V1\Models\Block;
 use Aparlay\Core\Models\Enums\UserStatus;
 use Aparlay\Core\Models\User;
-use Illuminate\Testing\Fluent\AssertableJson;
-use MongoDB\BSON\ObjectId;
+
 
 class UserTest extends ApiTestCase
 {
