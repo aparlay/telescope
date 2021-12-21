89f92a7862e85f9b8f0c57cc69bdd25a9fc6981f
andy bogdanov <jehkinen@ya.ru>
2021-12-21 18:45:15 +0500

    UserDocument structure

---
+++ src/Api/V1/Controllers/UserDocumentController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Aparlay\Core\Api\V1\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class UserDocumentController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request): Response
+    {
+        return $this->response([], Response::HTTP_OK);
+    }
+}
