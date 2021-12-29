2dbbaa2f0c14f09b70782824d4795e5521ce215d
andy bogdanov <jehkinen@ya.ru>
2021-12-29 16:34:15 +0500

    Added reject reason for admin

--- resources/views/admin/pages/user/tabs/_documents-lightbox.blade.php
+++ resources/views/admin/pages/user/tabs/_documents-lightbox.blade.php
@@ -22,14 +22,17 @@
 
                 <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="{{ '#approve_modal_' . $document->id }}">
                     <i class="fas fa-check"></i>
-                    <strong>Approve</strong>
+                    <strong>Confirm</strong>
                 </button>
 
                 <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="{{ '#reject_modal_' . $document->id }}">
-                    <i class="fas fa-check"></i>
+                    <i class="fas fa-times"></i>
                     <strong>Reject</strong>
                 </button>
 
+                @if($document->status === \Aparlay\Core\Models\Enums\UserDocumentStatus::REJECTED->value)
+                    <p><b>Reject Reason:</b> {{$document->reject_reason}}</p>
+                @endif
             </div>
 
             @include('default_view::admin.pages.user.tabs._documents-approve_modal', compact('document'))
