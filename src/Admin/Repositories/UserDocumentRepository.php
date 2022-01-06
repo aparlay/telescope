f3f5adcdba5ef9f286d6a45995e451f5b2884cd2
andy bogdanov <jehkinen@ya.ru>
2022-01-06 17:46:13 +0500

    added AbstractBaseRepo

--- src/Admin/Repositories/UserDocumentRepository.php
+++ src/Admin/Repositories/UserDocumentRepository.php
@@ -4,50 +4,7 @@ namespace Aparlay\Core\Admin\Repositories;
 
 use Aparlay\Core\Api\V1\Models\UserDocument;
 
-class UserDocumentRepository
+class UserDocumentRepository extends AbstractBaseRepository
 {
     protected static $model = UserDocument::class;
-
-    /**
-     * @param $offset
-     * @param $limit
-     * @param $sort
-     * @return mixed
-     */
-    public function all($offset, $limit, $sort)
-    {
-        return static::$model::sortBy($sort)
-            ->skip($offset)
-            ->take($limit)
-            ->get();
-    }
-
-    /**
-     * @param $offset
-     * @param $limit
-     * @param $sort
-     * @param $filters
-     * @return mixed
-     */
-    public function getFiltered($offset, $limit, $sort, $filters)
-    {
-        $query = static::$model::filter($filters)
-            ->sortBy($sort)
-            ->skip($offset)
-            ->take($limit);
-
-        $result = $query->get();
-
-        return $result;
-    }
-
-    public function countAll()
-    {
-        return static::$model::count();
-    }
-
-    public function countFiltered($filters)
-    {
-        return static::$model::filter($filters)->count();
-    }
 }
