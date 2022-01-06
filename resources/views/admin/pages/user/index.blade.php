20c86c6827e605d5e5856bbd6787d8122387c324
andy bogdanov <jehkinen@ya.ru>
2022-01-06 17:24:23 +0500

    reverted user documents table

--- resources/views/admin/pages/user/index.blade.php
+++ resources/views/admin/pages/user/index.blade.php
@@ -1,8 +1,7 @@
 @extends('adminlte::page')
 @section('title', 'Users')
 @section('css')
-    <link rel="stylesheet" href="{{ asset('vendor/daterangepicker/daterangepicker.css') }}" >
-    <link rel="stylesheet" href="{{ asset('admin/assets/css/adminStyles.css') }}" >
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/user.css') }}">
 @stop
 @section('plugins.Datatables', true)
 @section('content_header')
@@ -19,136 +18,122 @@
     </div><!-- /.row -->
 @stop
 @section('content')
-        <div class="content">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12 table-responsive">
-                        @php
-                            $heads = [
-                                '',
-                                'Username',
-                                'Email',
-                                'Fullname',
-                                '',
-                                'Status',
-                                'Documents',
-                                'Visibility',
-                                'Followers',
-                                'Likes',
-                                'Medias',
-                                '',
-                                'Created at',
-                                '',
-                            ];
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 table-responsive">
+                    @php
+                        $heads = [
+                            '',
+                            'Username',
+                            'Email',
+                            'Fullname',
+                            '',
+                            'Status',
+                            'Visibility',
+                            'Followers',
+                            'Likes',
+                            'Medias',
+                            'Created at',
+                            '',
+                            ''
+                        ];
 
-                        $config = [
-                            'processing' => true,
-                            'serverSide' => true,
-                            'pageLength' => config('core.admin.lists.page_count'),
-                            'responsive' => true,
-                            'lengthChange' => false,
-                            'dom' => 'rtip',
-                            'orderMulti' => false,
-                            'autoWidth' => false,
-                            'ajax' => route('core.admin.ajax.user.index'),
-                            'order' => [[11, 'desc']],
-                            'columns' => [
-                                ['data' => 'username', 'visible' => false],
-                                ['data' => 'username_avatar', 'orderData' => 0, 'target' => 0],
-                                ['data' => 'email'],
-                                ['data' => 'full_name', 'orderable' => false],
-                                ['data' => 'status', 'visible' => false],
-                                ['data' => 'status_badge', 'orderData' => 4, 'target' => 4],
-                                ['data' => 'documents', 'orderable' => false],
-                                ['data' => 'visibility'],
-                                ['data' => 'follower_count', 'orderable' => false],
-                                ['data' => 'like_count', 'orderable' => false],
-                                ['data' => 'media_count', 'orderable' => false],
-                                ['data' => 'created_at', 'visible' => false],
-                                ['data' => 'date_formatted', 'orderData' => 11, 'target' => 11],
-                                ['data' => 'action', 'orderable' => false],
-                            ],
-                        ]
-                        @endphp
-                        <div id="accordion">
-                            <div class="card card-primary">
-                                <div class="card-header">
-                                    <h4 class="card-title w-100">
-                                        <a class="d-block w-100" data-toggle="collapse" href="#collapseOne" aria-expanded="true">
-                                            Show/Hide Filter
-                                        </a>
-                                    </h4>
-                                </div>
-                                <div id="collapseOne" class="collapse" data-parent="#accordion" style="">
-                                    <div class="card-body">
-                                        <form action="" id="filters">
-                                            <div class="row">
-                                                <div class="col-sm-3">
-                                                    <div class="form-group">
-                                                        <label for="username">Username</label>
-                                                        <input type="text" data-column="0" name="username" class="form-control" id="username" placeholder="Enter username">
-                                                    </div>
-                                                </div>
-                                                <div class="col-sm-3">
-                                                    <div class="form-group">
-                                                        <label for="email">Email</label>
-                                                        <input type="email" data-column="2" name="email" class="form-control" id="email" placeholder="Enter email">
-                                                    </div>
-                                                </div>
-                                                <div class="col-sm-3">
-                                                    <div class="form-group">
-                                                        <label for="status">Status</label>
-                                                        <select name="status" data-column="4" id="status" class="form-control">
-                                                            <option value="">-Select-</option>
-                                                            @foreach($userStatuses as $key => $status)
-                                                                <option value="{{ $key }}">{{ $status }}</option>
-                                                            @endforeach
-                                                        </select>
-                                                    </div>
-                                                </div>
-                                                <div class="col-sm-3">
-                                                    <div class="form-group">
-                                                        <label for="visibility">Visibility</label>
-                                                        <select name="visibility" data-column="7" id="visibility" class="form-control">
-                                                            <option value="">-Select-</option>
-                                                            @foreach($userVisibilities as $key => $visibility)
-                                                                <option value="{{ $key }}">{{ $visibility }}</option>
-                                                            @endforeach
-                                                        </select>
-                                                    </div>
-                                                </div>
-                                                <div class="col-sm-3">
-                                                    <div class="form-group">
-                                                        <label for="documents">Has documents with status</label>
-                                                        <select name="documents" data-column="6" id="documents" class="form-control">
-                                                            <option value="">-Select-</option>
-                                                            @foreach($userDocumentStatuses as $key => $value)
-                                                                <option value="{{ $key }}">{{ $value }}</option>
-                                                            @endforeach
-                                                        </select>
-                                                    </div>
+                    $config = [
+                        'processing' => true,
+                        'serverSide' => true,
+                        'pageLength' => config('core.admin.lists.page_count'),
+                        'responsive' => true,
+                        'lengthChange' => false,
+                        'dom' => 'rtip',
+                        'orderMulti' => false,
+                        'autoWidth' => false,
+                        'ajax' => route('core.admin.ajax.user.index'),
+                        'order' => [[10, 'desc']],
+                        'columns' => [
+                            ['data' => 'username', 'visible' => false],
+                            ['data' => 'username_avatar', 'orderData' => 0, 'target' => 0],
+                            ['data' => 'email'],
+                            ['data' => 'full_name', 'orderable' => false],
+                            ['data' => 'status', 'visible' => false],
+                            ['data' => 'status_badge', 'orderData' => 4, 'target' => 4],
+                            ['data' => 'visibility'],
+                            ['data' => 'follower_count', 'orderable' => false],
+                            ['data' => 'like_count', 'orderable' => false],
+                            ['data' => 'media_count', 'orderable' => false],
+                            ['data' => 'created_at', 'visible' => false],
+                            ['data' => 'date_formatted', 'orderData' => 10, 'target' => 10],
+                            ['data' => 'action', 'orderable' => false],
+                        ],
+                    ]
+                    @endphp
+                    <div id="accordion">
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h4 class="card-title w-100">
+                                    <a class="d-block w-100" data-toggle="collapse" href="#collapseOne" aria-expanded="true">
+                                        Show/Hide Filter
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapseOne" class="collapse" data-parent="#accordion" style="">
+                                <div class="card-body">
+                                    <form action="" id="filters">
+                                        <div class="row">
+                                            <div class="col-sm-3">
+                                                <div class="form-group">
+                                                    <label for="username">Username</label>
+                                                    <input type="text" data-column="0" name="username" class="form-control" id="username" placeholder="Enter username">
                                                 </div>
                                             </div>
-                                            @include('default_view::admin.parts.date-range-filter', ['column' => 10])
-                                            <div class="row d-flex justify-content-end">
-                                                <div class="col-1">
-                                                    <button type="button" id="clearFilter" class="btn btn-block btn-danger"><i class="fas fa-trash"></i> Clear</button>
-                                                </div>
-                                                <div class="col-1">
-                                                    <button type="button" id="submitFilter" class="btn btn-block btn-primary"><i class="fas fa-filter"></i> Filter</button>
+                                            <div class="col-sm-3">
+                                                <div class="form-group">
+                                                    <label for="email">Email</label>
+                                                    <input type="email" data-column="2" name="email" class="form-control" id="email" placeholder="Enter email">
                                                 </div>
                                             </div>
-                                        </form>
-                                    </div>
+                                            <div class="col-sm-3">
+                                                <div class="form-group">
+                                                    <label for="status">Status</label>
+                                                    <select name="status" data-column="4" id="status" class="form-control">
+                                                        <option value="">-Select-</option>
+                                                        @foreach($userStatuses as $key => $status)
+                                                            <option value="{{ $key }}">{{ $status }}</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-3">
+                                                <div class="form-group">
+                                                    <label for="visibility">Visibility</label>
+                                                    <select name="visibility" data-column="6" id="visibility" class="form-control">
+                                                        <option value="">-Select-</option>
+                                                        @foreach($userVisibilities as $key => $visibility)
+                                                            <option value="{{ $key }}">{{ $visibility }}</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row d-flex justify-content-end">
+                                            <div class="col-1">
+                                                <button type="button" id="clearFilter" class="btn btn-block btn-danger"><i class="fas fa-trash"></i> Clear</button>
+                                            </div>
+                                            <div class="col-1">
+                                                <button type="button" id="submitFilter" class="btn btn-block btn-primary"><i class="fas fa-filter"></i> Filter</button>
+                                            </div>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
-                        <x-adminlte-datatable id="datatables" :heads="$heads" :config="$config">
-                        </x-adminlte-datatable>
                     </div>
+                    <x-adminlte-datatable id="datatables" :heads="$heads" :config="$config">
+                    </x-adminlte-datatable>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 @endsection
 @section('js')
