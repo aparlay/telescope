ef7e9b06e19dd64b7d9ba826abd9980debe9e06f
Ramin Farmani <ramin.farmani@gmail.com>
2022-01-12 15:48:35 +0330

    Fix implementation

--- database/migrations/2022_01_12_120820_add_user_columns.php
+++ database/migrations/2022_01_12_120820_add_user_columns.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddUserColumns extends Migration
+class AddStatsUserColumns extends Migration
 {
     /**
      * Run the migrations.
