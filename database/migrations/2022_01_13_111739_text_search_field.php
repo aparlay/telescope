6c9cf88f49451bbbacecb9fbae58172b15e17235
Ramin Farmani <ramin.farmani@gmail.com>
2022-01-13 14:52:44 +0330

    fix search

--- database/migrations/2022_01_13_111739_text_search_field.php
+++ database/migrations/2022_01_13_111739_text_search_field.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UserTextSearchField extends Migration
+class TextSearchField extends Migration
 {
     /**
      * Run the migrations.
