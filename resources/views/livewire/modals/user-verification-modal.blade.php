8634685ca7a885993721eada424cd122fe95cf68
Andy Bogdanov <jehkinen@ya.ru>
2022-01-19 20:23:29 +0500

    implemented approve/reject modal for user moderation lists

--- resources/views/livewire/modals/user-verification-modal.blade.php
+++ resources/views/livewire/modals/user-verification-modal.blade.php
@@ -5,7 +5,7 @@
 <div class="modal-dialog" role="document">
     <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Approve user</h5>
+            <h5 class="modal-title">Approve user</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true close-btn">×</span>
             </button>
