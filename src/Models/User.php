da0466e66fd3c9b653c0870820292545ed939ab8
Ramin Farmani <ramin.farmani@gmail.com>
2022-01-22 08:35:57 +0330

    Fix bug

--- src/Models/User.php
+++ src/Models/User.php
@@ -591,23 +591,30 @@ class User extends Authenticatable implements JWTSubject
 
     public function getCountryLabelAttribute()
     {
-        return CountryHelper::getNameByAlpha2($this->country_alpha2);
+        return $this->country_alpha2 ? CountryHelper::getNameByAlpha2($this->country_alpha2) : '';
     }
 
     public function getCountryFlagAttribute()
     {
-        return CountryHelper::getFlagByAlpha2($this->country_alpha2);
+        return $this->country_alpha2 ? CountryHelper::getFlagByAlpha2($this->country_alpha2) : '';
     }
 
     public function getCountryFlagsAttribute()
     {
-        return [
+        return $this->country_alpha2 ? [
             '16' => CountryHelper::getFlagByAlpha2($this->country_alpha2, '16'),
             '24' => CountryHelper::getFlagByAlpha2($this->country_alpha2, '24'),
             '32' => CountryHelper::getFlagByAlpha2($this->country_alpha2, '32'),
             '48' => CountryHelper::getFlagByAlpha2($this->country_alpha2, '48'),
             '64' => CountryHelper::getFlagByAlpha2($this->country_alpha2, '64'),
             '128' => CountryHelper::getFlagByAlpha2($this->country_alpha2, '128'),
+        ] : [
+            '16' => '',
+            '24' => '',
+            '32' => '',
+            '48' => '',
+            '64' => '',
+            '128' => '',
         ];
     }
 }
