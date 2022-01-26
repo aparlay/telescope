2919554bffcde7522da11d1bc44d93cfa10c77c2
jayson-suizo <jayson.b.suizo@gmail.com>
2022-01-26 15:29:31 +0800

    update seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,30 +17,30 @@ class DatabaseSeeder extends Seeder
     {
         Media::unsetEventDispatcher();
 
-        // DB::collection('alerts')->truncate();
-        // DB::collection('analytics')->truncate();
-        // DB::collection('blocks')->truncate();
-        // DB::collection('emails')->truncate();
-        // DB::collection('medias')->truncate();
-        // DB::collection('media_likes')->truncate();
-        // DB::collection('media_visits')->truncate();
-        // DB::collection('users')->truncate();
-        // DB::collection('versions')->truncate();
-        // DB::collection('user_documents')->truncate();
+        DB::collection('alerts')->truncate();
+        DB::collection('analytics')->truncate();
+        DB::collection('blocks')->truncate();
+        DB::collection('emails')->truncate();
+        DB::collection('medias')->truncate();
+        DB::collection('media_likes')->truncate();
+        DB::collection('media_visits')->truncate();
+        DB::collection('users')->truncate();
+        DB::collection('versions')->truncate();
+        DB::collection('user_documents')->truncate();
         DB::collection('notes')->truncate();
 
         $this->call([
-            // AlertSeeder::class,
-            // AnalyticSeeder::class,
-            // BlockSeeder::class,
-            // EmailSeeder::class,
-            // FollowSeeder::class,
-            // MediaSeeder::class,
-            // MediaLikeSeeder::class,
-            // MediaVisitSeeder::class,
-            // UserSeeder::class,
-            // UserDocumentSeeder::class,
-            // VersionSeeder::class,
+            AlertSeeder::class,
+            AnalyticSeeder::class,
+            BlockSeeder::class,
+            EmailSeeder::class,
+            FollowSeeder::class,
+            MediaSeeder::class,
+            MediaLikeSeeder::class,
+            MediaVisitSeeder::class,
+            UserSeeder::class,
+            UserDocumentSeeder::class,
+            VersionSeeder::class,
             NoteSeeder::class,
         ]);
     }
