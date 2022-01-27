e6d4815974eeca22f9967b6ffbd2acba853f9105
farmani <farmani@users.noreply.github.com>
2022-01-27 07:13:29 +0000

    Fix styling

--- src/Admin/Filters/FilterPartial.php
+++ src/Admin/Filters/FilterPartial.php
@@ -15,6 +15,6 @@ class FilterPartial extends AbstractBaseFilter
 
     public function __invoke($query)
     {
-        $query->where($this->getInternalFieldName(), 'like', '%' . $this->fieldValue . '%');
+        $query->where($this->getInternalFieldName(), 'like', '%'.$this->fieldValue.'%');
     }
 }
