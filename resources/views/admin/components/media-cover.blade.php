46e0dab6b5332271f181dd613ff6a8f42cb78740
Ramin Farmani <ramin.farmani@gmail.com>
2022-01-27 16:06:55 +0330

    Fix media cover link

--- resources/views/admin/components/media-cover.blade.php
+++ resources/views/admin/components/media-cover.blade.php
@@ -1,4 +1,4 @@
 <a href="{{$media->admin_url}}">
-    <img class="img-thumbnail" src="{{$media->cover_url}}?aspect_ratio=1:1&width=150"/>
+    <img class="img-thumbnail" src="{{$media->cover_url}}?aspect_ratio=1:1&width=100"/>
 </a>
 
