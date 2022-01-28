45eda125afda4e6775d51d4a9ec0d5ac4dc904bf
jayson-suizo <jayson.b.suizo@gmail.com>
2022-01-28 17:11:38 +0800

    delete some config

--- config/livewire-components.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'users-table' => 'Aparlay\\Core\\Admin\\Livewire\\UsersTable',
-    'modals.user-verification-modal' => 'Aparlay\\Core\\Admin\\Livewire\\Modals\\UserVerificationModal',
-    'notes-table' => 'Aparlay\\Core\\Admin\\Livewire\\NotesTable',
-];
