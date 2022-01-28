5efbc720fdf0a6b8da7ebd6c5b95cde1a8e32154
jayson-suizo <jayson-suizo@users.noreply.github.com>
2022-01-28 11:07:16 +0000

    Fix styling

--- src/Admin/Services/NoteService .php
+++ src/Admin/Services/NoteService .php
@@ -2,22 +2,19 @@
 
 namespace Aparlay\Core\Admin\Services;
 
-use Aparlay\Core\Models\Note;
 use Aparlay\Core\Admin\Repositories\NoteRepository;
+use Aparlay\Core\Models\Note;
 
 class AlertService
 {
     protected NoteRepository $noteRepository;
-   
 
     public function __construct()
     {
         $this->noteRepository = new NoteRepository(new Note());
-        
     }
 
     public function create()
     {
-       
     }
 }
