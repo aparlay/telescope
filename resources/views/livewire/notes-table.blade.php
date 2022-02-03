e6c80b1423598b034bdcee7a3274d735761bc5cd
Andy Bogdanov <jehkinen@ya.ru>
2022-02-03 16:22:45 +0500

    -php block for user-notes-create-modal.blade.php

--- resources/views/livewire/notes-table.blade.php
+++ resources/views/livewire/notes-table.blade.php
@@ -1,104 +1,111 @@
-
 <div class="notes-table">
 
-    <div class="col-md-2 ml-auto text-right">
-        <button
-            class="btn btn-md btn-success"
-            type="button"
-            wire:click="$emit('showModal', 'modals.user-notes-create-modal', '{{ $userId }}')"
-        >
-            Add New
-            <i class="fas fa-plus fa-xs"></i>
-        </button>
-    </div>
     <div class="filters pb-3">
         <div class="row">
-            <div class="col-md-1">
-                <label for="">Start Date</label>
-                <x-date-picker
-                        wire:model.lazy="filter.created_at.start"
-                        autocomplete="off"
-                        placeholder="Start"
-                />
-            </div>
-            <div class="col-md-1">
-                <label for="">End Date</label>
-                <x-date-picker
-                        wire:model.lazy="filter.created_at.end"
-                        autocomplete="off"
-                        placeholder="End"
-                />
+            <div class="col-md-2 text-left">
+                <label for="" style="color: #fff">
+                    Create a new note
+                </label>
+                <button
+                    class="btn btn-md btn-success"
+                    type="button"
+                    wire:click="$emit('showModal', 'modals.user-notes-create-modal', '{{ $userId }}')"
+                >
+                    Add New
+                    <i class="fas fa-plus fa-xs"></i>
+                </button>
             </div>
 
-            <div class="col-md-2 ml-auto">
-                <label for="">Per Page</label>
-                <x-wire-dropdown-list :wire-model="'perPage'" :show-any="false" :options="[5 => 5, 10 => 10, 15 => 15]"/>
+            <div class="col-md-3 ml-auto">
+                <div class="row">
+                    <div class="col">
+                        <label for="">Start Date</label>
+                        <x-date-picker
+                            wire:model.lazy="filter.created_at.start"
+                            autocomplete="off"
+                            placeholder="Start"
+                        />
+                    </div>
+                    <div class="col">
+                        <label for="">End Date</label>
+                        <x-date-picker
+                            wire:model.lazy="filter.created_at.end"
+                            autocomplete="off"
+                            placeholder="End"
+                        />
+                    </div>
+                    <div class="col">
+                        <label for="">Per Page</label>
+                        <x-wire-dropdown-list :wire-model="'perPage'" :show-any="false" :options="[5 => 5, 10 => 10, 15 => 15]"/>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+</div>
 
-    <table class="table table-striped">
-        <tbody>
+<table class="table table-striped">
+    <tbody>
+    <tr>
+        <th class="col-md-2">
+            <div>
+
+                <x-sortable-column-header :sort="$sort" :fieldName="'created_by'" :fieldLabel="'Created By'"/>
+                <input class="form-control" type="text" wire:model="filter.creator_username"/>
+            </div>
+        </th>
+        <th class="col-md-6">
+            <div>
+                <x-sortable-column-header :sort="$sort" :fieldName="'message'" :fieldLabel="'Notes'"/>
+                <input class="form-control" type="text" wire:model="filter.message"/>
+            </div>
+        </th>
+        <th class="col-md-3">
+            <div>
+                <x-sortable-column-header :sort="$sort" :fieldName="'created_at'" :fieldLabel="'Created At'"/>
+            </div>
+        </th>
+        <th class="col-md-1">
+            <div>
+                <label for="">Action</label>
+
+            </div>
+        </th>
+        <th></th>
+    </tr>
+
+    @foreach($notes as $note)
         <tr>
-            <th class="col-md-2">
-                <div>
-
-                    <x-sortable-column-header :sort="$sort" :fieldName="'created_by'" :fieldLabel="'Created By'" />
-                    <input class="form-control" type="text" wire:model="filter.creator_username"/>
-                </div>
-            </th>
-            <th class="col-md-6">
-                <div>
-                    <x-sortable-column-header :sort="$sort" :fieldName="'message'" :fieldLabel="'Notes'" />
-                    <input class="form-control" type="text" wire:model="filter.message"/>
-                </div>
-            </th>
-            <th class="col-md-3">
-                <div>
-                    <x-sortable-column-header :sort="$sort" :fieldName="'created_at'" :fieldLabel="'Created At'" />
-                </div>
-            </th>
-            <th class="col-md-1">
-                <div>
-                    <label for="">Action</label>
-
-                </div>
-            </th>
-            <th></th>
-        </tr>
-
-        @foreach($notes as $note)
-            <tr>
-                <td>
-                    <a href="">
+            <td>
+                <a href="">
 
                     {{ $note->creator['username'] }}
 
-                    </a>
-                </td>
+                </a>
+            </td>
 
-                <td>
-                    {!! $note->message !!}
-                </td>
-                <td>
-                    {{ $note->created_at }}
-                </td>
-                <td>
-                    <div>
-                        <button
-                            class="btn btn-sm btn-danger"
-                            type="button"
-                            wire:key="delete_note_{{ $note->_id }}}"
-                            wire:click="$emit('showModal', 'modals.user-notes-delete-modal', '{{ $note->_id }}')"
-                        >
-                            <i class="fa fa-trash"></i>
-                        </button>
-                    </div>
-                </td>
-            </tr>
+            <td>
+                {!! $note->message !!}
+            </td>
+            <td>
+                {{ $note->created_at }}
+            </td>
+            <td>
+                <div>
+                    <button
+                        class="btn btn-sm btn-danger"
+                        type="button"
+                        wire:key="delete_note_{{ $note->_id }}}"
+                        wire:click="$emit('showModal', 'modals.user-notes-delete-modal', '{{ $note->_id }}')"
+                    >
+                        <i class="fa fa-trash"></i>
+                    </button>
+                </div>
+            </td>
+        </tr>
 
-        @endforeach
-        </tbody>
-    </table>
-    {{ $notes->links() }}
+    @endforeach
+    </tbody>
+</table>
+{{ $notes->links() }}
 </div>
