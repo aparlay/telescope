1cc9af769218155a77cfb34cf5f9a74cf5428ae7
Andy Bogdanov <jehkinen@ya.ru>
2022-02-03 13:13:38 +0500

    -unused imports for UserNoteDeleteModal.php

--- src/Admin/Livewire/Modals/UserNoteDeleteModal.php
+++ src/Admin/Livewire/Modals/UserNoteDeleteModal.php
@@ -3,16 +3,7 @@
 namespace Aparlay\Core\Admin\Livewire\Modals;
 
 use Aparlay\Core\Admin\Livewire\Traits\CurrentUserTrait;
-use Aparlay\Core\Admin\Models\Alert;
 use Aparlay\Core\Admin\Models\Note;
-use Aparlay\Core\Admin\Models\User;
-use Aparlay\Core\Admin\Repositories\AlertRepository;
-use Aparlay\Core\Admin\Repositories\UserRepository;
-use Aparlay\Core\Constants\Roles;
-use Aparlay\Core\Models\Enums\AlertStatus;
-use Aparlay\Core\Models\Enums\AlertType;
-use Aparlay\Core\Models\Enums\UserDocumentStatus;
-use Illuminate\Validation\Rule;
 use Livewire\Component;
 
 class UserNoteDeleteModal extends Component
