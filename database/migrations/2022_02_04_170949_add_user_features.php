17fb9e94738093799c2871142bf9735c0ec1e4eb
Ramin Farmani <ramin.farmani@gmail.com>
2022-02-04 20:44:38 +0330

    Adding date scope to the user

---
+++ database/migrations/2022_02_04_170949_add_user_features.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserFeatures extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        User::where('features.wallet_cryptocurrency', null)->update([
+            'features' => [
+                'tips' => false,
+                'exclusive_content' => false,
+                'wallet_bank' => false,
+                'wallet_paypal' => false,
+                'wallet_cryptocurrency' => false,
+                'demo' => false,
+            ],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
