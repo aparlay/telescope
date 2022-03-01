020b2d940e5d2df6b68a37e55fb886a2d570748b
Andrey Bogdanov <jehkinen@ya.ru>
2022-03-01 11:29:41 +0500

    added events to send otp code request

---
+++ src/Events/UserOtpRequestedEvent.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Aparlay\Core\Events;
+
+
+use Aparlay\Core\Models\User;
+use Illuminate\Auth\Events\Authenticated;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class UserOtpRequestedEvent
+{
+    use Dispatchable;
+    use SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        protected Authenticated|User $user,
+        protected $deviceId
+    )
+    {}
+
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    public function getDeviceId()
+    {
+        return $this->deviceId;
+    }
+
+}
---
+++ src/Listeners/SendOtpToUserListener.php
@@ -0,0 +1,19 @@
+<?php
+namespace Aparlay\Core\Listeners;
+
+use Aparlay\Core\Api\V1\Services\OtpService;
+use Aparlay\Core\Events\UserOtpRequestedEvent;
+
+
+class SendOtpToUserListener
+{
+
+    public function __construct(
+        private OtpService $otpService
+    ){}
+
+    public function handle(UserOtpRequestedEvent $event)
+    {
+        $this->otpService->sendOtp($event->getUser(), $event->getDeviceId());
+    }
+}
