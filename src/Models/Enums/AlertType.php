3d706acb6e990562617b4588d9ed46e4b3557bd7
Andrey Bogdanov <jehkinen@ya.ru>
2022-04-12 14:14:08 +0500

    changed logic for user payouts

--- src/Models/Enums/AlertType.php
+++ src/Models/Enums/AlertType.php
@@ -11,7 +11,7 @@ enum AlertType: int implements Enum
     case MEDIA_NOTICED = 21;
 
     case USER_DOCUMENT_REJECTED = 50;
-    case USER_PAYOUT_REJECTED = 30;
+    case USER_PAYOUT_DELETED = 30;
     case USER_PAYOUT_FAILED = 31;
     case USER_PAYOUT_COMPLETED_MANUALLY = 32;
 
@@ -24,7 +24,7 @@ enum AlertType: int implements Enum
             self::MEDIA_REMOVED => __('media removed'),
             self::MEDIA_NOTICED => __('media noticed'),
             self::USER_DOCUMENT_REJECTED => __('user document rejected'),
-            self::USER_PAYOUT_REJECTED => __('user payout rejected')
+            self::USER_PAYOUT_DELETED => __('user payout rejected')
         };
     }
 
@@ -32,7 +32,7 @@ enum AlertType: int implements Enum
     {
         return match ($this) {
             self::USER => 'warning',
-            self::MEDIA_REMOVED, self::USER_DOCUMENT_REJECTED, self::USER_PAYOUT_REJECTED => 'danger',
+            self::MEDIA_REMOVED, self::USER_DOCUMENT_REJECTED, self::USER_PAYOUT_DELETED => 'danger',
             self::MEDIA_NOTICED => 'info'
         };
     }
