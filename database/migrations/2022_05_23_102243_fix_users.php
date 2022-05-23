389b9c91e7c34a4daad1c287e42872b9f868cc62
Ramin Farmani <ramin.farmani@gmail.com>
2022-05-23 15:02:02 +0430

    Fix bug in code app

---
+++ database/migrations/2022_05_23_102243_fix_users.php
@@ -0,0 +1,54 @@
+<?php
+
+use Aparlay\Core\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach (User::lazy() as $user) {
+            if (empty($user->setting)) {
+                $user->update(['setting' => [
+                    'otp' => false,
+                    'notifications' => [
+                        'unread_message_alerts' => false,
+                        'new_followers' => false,
+                        'news_and_updates' => false,
+                        'tips' => false,
+                        'new_subscribers' => false,
+                    ],
+                    'risk' => [
+                        'block_unverified_cc' => false,
+                        'spent_amount' => 0,
+                    ]
+                ]]);
+            }
+            if (empty($user->setting['risk'])) {
+                $setting = $user->setting;
+                $setting['risk'] = [
+                    'block_unverified_cc' => false,
+                    'spent_amount' => 0,
+                ];
+                $user->update(['setting' => $setting]);
+            }
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
