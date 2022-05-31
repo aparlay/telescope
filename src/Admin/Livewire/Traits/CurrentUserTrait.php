e90829f7fd1b6a010b0b2a8b99be63f005388b9a
Ramin Farmani <ramin.farmani@gmail.com>
2022-05-31 12:51:47 +0430

    Fix chat modal service

--- src/Admin/Livewire/Traits/CurrentUserTrait.php
+++ src/Admin/Livewire/Traits/CurrentUserTrait.php
@@ -2,15 +2,10 @@
 
 namespace Aparlay\Core\Admin\Livewire\Traits;
 
-use Aparlay\Core\Admin\Models\User;
 use Auth;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 trait CurrentUserTrait
 {
-
-    use AuthorizesRequests;
-
     public function currentUser()
     {
         //$currentUserId = Auth::guard('admin')->user()->id();
