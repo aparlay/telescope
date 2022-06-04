50259553c5c5b83934072ace3a78e181443c1041
farmani <farmani@users.noreply.github.com>
2022-06-04 11:53:14 +0000

    Fix styling

--- database/migrations/2022_06_04_110837_fix_user.php
+++ database/migrations/2022_06_04_110837_fix_user.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class() extends Migration {
     /**
      * Run the migrations.
      *
@@ -16,7 +15,7 @@ return new class extends Migration
     {
         foreach (User::lazy() as $user) {
             $setting = $user->setting;
-            if (!isset($user->setting['payment'])) {
+            if (! isset($user->setting['payment'])) {
                 $setting['payment'] = [
                     'allow_unverified_cc' => false,
                     'block_unverified_cc' => false,
