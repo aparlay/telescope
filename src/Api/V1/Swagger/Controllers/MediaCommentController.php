ed2497bd1dbaefa4a4759b4e2efa2121197b0f46
Andrey Bogdanov <jehkinen@ya.ru>
2022-06-17 12:27:40 +0500

    swagger docs for media comments

---
+++ src/Api/V1/Swagger/Controllers/MediaCommentController.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Aparlay\Core\Api\V1\Swagger\Controllers;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Post(
+ *     path="/media/:mediaId/comment",
+ *     tags={"Core | Media Comment"},
+ *     summary="Create media comment",
+ *     description="To like media you need to call this endpoint.",
+ *     operationId="createMediaCOmment",
+ *     security={{"bearerAuth": {}}},
+ *     @OA\Parameter(
+ *         name="mediaId",
+ *         in="path",
+ *         description="media id to create a new comment",
+ *         required=true,
+ *         @OA\Schema(
+ *             type="string"
+ *         )
+ *     ),
+ *     @OA\Parameter(
+ *         name="X-DEVICE-ID",
+ *         in="header",
+ *         description="unique id of the device user is going to send this request it can be segment.com anonymousId.",
+ *         required=true,
+ *         @OA\Schema(
+ *             type="string"
+ *         )
+ *     ),
+ *      @OA\RequestBody(
+ *          required=true,
+ *          description="",
+ *          @OA\JsonContent(
+ *           required={"text"},
+ *             @OA\Property(property="text", type="string",  example="May you find your Tower, Roland, and breach it, and may you climb to the top!"),
+ *          ),
+ *      ),
+ *     @OA\Response(
+ *         response=201,
+ *         description="successful operation",
+ *         @OA\Header(
+ *             header="X-Rate-Limit-Limit",
+ *             description="the maximum number of allowed requests during a period",
+ *             @OA\Schema(
+ *                 type="integer",
+ *                 format="int32"
+ *             )
+ *         ),
+ *         @OA\Header(
+ *             header="X-Rate-Limit-Remaining",
+ *             description="the remaining number of allowed requests within the current period",
+ *             @OA\Schema(
+ *                 type="integer",
+ *                 format="int32"
+ *             )
+ *         ),
+ *         @OA\Header(
+ *             header="X-Rate-Limit-Reset",
+ *             description="the number of seconds to wait before having maximum number of allowed requests again",
+ *             @OA\Schema(
+ *                 type="integer",
+ *                 format="int32"
+ *             )
+ *         ),
+ *         @OA\JsonContent(ref="#/components/schemas/MediaComment"),
+ *     ),
+ *     @OA\Response(
+ *         response=401,
+ *         description="Unauthorized",
+ *         @OA\JsonContent(ref="#/components/schemas/401"),
+ *     ),
+ *     @OA\Response(
+ *         response=422,
+ *         description="DATA VALIDATION FAILED",
+ *         @OA\JsonContent(ref="#/components/schemas/422"),
+ *     ),
+ *     @OA\Response(
+ *         response=429,
+ *         description="TOO MANY REQUESTS",
+ *         @OA\JsonContent(ref="#/components/schemas/429"),
+ *     ),
+ * )
+ *
+ * @OA\Delete (
+ *     path="/v1/media-comment/{id}",
+ *     tags={"Core | Media Comment"},
+ *     summary="Delete media comment by id",
+ *     description="To delete media comment call this endpoint",
+ *     operationId="deleteMediaComment",
+ *     security={{"bearerAuth": {}}},
+ *     @OA\Parameter(
+ *         name="id",
+ *         in="path",
+ *         description="media comment id to delete.",
+ *         required=true,
+ *         @OA\Schema(
+ *             type="string"
+ *         )
+ *     ),
+ *     @OA\Parameter(
+ *         name="X-DEVICE-ID",
+ *         in="header",
+ *         description="unique id of the device user is going to send this request it can be segment.com anonymousId.",
+ *         required=true,
+ *         @OA\Schema(
+ *             type="string"
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=204,
+ *         description="successful operation",
+ *         @OA\Header(
+ *             header="X-Rate-Limit-Limit",
+ *             description="the maximum number of allowed requests during a period",
+ *             @OA\Schema(
+ *                 type="integer",
+ *                 format="int32"
+ *             )
+ *         ),
+ *         @OA\Header(
+ *             header="X-Rate-Limit-Remaining",
+ *             description="the remaining number of allowed requests within the current period",
+ *             @OA\Schema(
+ *                 type="integer",
+ *                 format="int32"
+ *             )
+ *         ),
+ *         @OA\Header(
+ *             header="X-Rate-Limit-Reset",
+ *             description="the number of seconds to wait before having maximum number of allowed requests again",
+ *             @OA\Schema(
+ *                 type="integer",
+ *                 format="int32"
+ *             )
+ *         ),
+ *     ),
+ *     @OA\Response(
+ *         response=401,
+ *         description="Unauthorized",
+ *         @OA\JsonContent(ref="#/components/schemas/401"),
+ *     ),
+ *     @OA\Response(
+ *         response=422,
+ *         description="DATA VALIDATION FAILED",
+ *         @OA\JsonContent(ref="#/components/schemas/422"),
+ *     ),
+ *     @OA\Response(
+ *         response=429,
+ *         description="TOO MANY REQUESTS",
+ *         @OA\JsonContent(ref="#/components/schemas/429"),
+ *     )
+ * )
+ */
+class MediaCommentController
+{
+}
---
+++ src/Api/V1/Swagger/Resources/MediaCommentReply.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Aparlay\Core\Api\V1\Swagger\Resources;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Schema()
+ */
+class MediaCommentReply
+{
+    /**
+     * @OA\Property(property="_id", type="string", example="60237caf5e41025e1e3c80b1")
+     */
+    public $_id;
+
+    /**
+     * @OA\Property(property="media_id", type="string", example="60237caf5e41025e1e3c80b1")
+     */
+    public $media_id;
+
+    /**
+     * @OA\Property(property="text", type="string", example="In this universe there might grow roses which sing"),
+     */
+    public $text;
+
+    /**
+     * @OA\Property(property="is_liked", type="boolean", description="Is liked by current user", example="true")
+     */
+    public $is_liked;
+
+    /**
+     * @OA\Property(property="likes_count", type="int", description="Number of likes for this comment", example="5")
+     */
+    public $likes_count;
+
+    /**
+     * @OA\Property(property="replies_count", type="int", description="Number of replies for this comment", example="10")
+     */
+    public $replies_count;
+
+    /**
+     * @OA\Property(property="parent_id", type="string", example="60237caf5e41025e1e3c80b1", description="Only replies have parent_id")
+     */
+    public $parent_id;
+
+
+    /**
+     * @OA\Property(property="created_at", type="string", example="1612850111566")
+     */
+    public $created_at;
+
+    /**
+     * @OA\Property(property="creator", ref="#/components/schemas/SimpleUser")
+     */
+    public $creator;
+}
