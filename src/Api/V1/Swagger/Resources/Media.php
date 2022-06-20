73b248b9d29a7f216d46970a2e518f26ed29f0b6
jehkinen <jehkinen@users.noreply.github.com>
2022-06-20 13:04:56 +0000

    Fix styling

--- src/Api/V1/Swagger/Resources/Media.php
+++ src/Api/V1/Swagger/Resources/Media.php
@@ -119,13 +119,10 @@ class Media
      */
     public $is_adult;
 
-
     /**
      * @OA\Property(property="is_comments_enabled", type="boolean", example=true)
      */
     public $is_comments_enabled;
-
-
 
     /**
      * @OA\Property(property="slug", type="string", example="weER34")
