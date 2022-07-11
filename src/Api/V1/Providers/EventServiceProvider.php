93c559991530f851b63f84af0f9eb112c2e714ec
Ramin Farmani <ramin.farmani@gmail.com>
2022-07-11 12:32:17 +0430

    Remove sanctum for now

--- src/Api/V1/Providers/EventServiceProvider.php
+++ src/Api/V1/Providers/EventServiceProvider.php
@@ -2,6 +2,21 @@
 
 namespace Aparlay\Core\Api\V1\Providers;
 
+use Aparlay\Core\Api\V1\Models\Block;
+use Aparlay\Core\Api\V1\Models\Follow;
+use Aparlay\Core\Api\V1\Models\Media;
+use Aparlay\Core\Api\V1\Models\MediaLike;
+use Aparlay\Core\Api\V1\Models\MediaVisit;
+use Aparlay\Core\Api\V1\Models\Report;
+use Aparlay\Core\Api\V1\Models\User;
+use Aparlay\Core\Observers\BlockObserver;
+use Aparlay\Core\Observers\FollowObserver;
+use Aparlay\Core\Observers\MediaLikeObserver;
+use Aparlay\Core\Observers\MediaObserver;
+use Aparlay\Core\Observers\MediaVisitObserver;
+use Aparlay\Core\Observers\ReportObserver;
+use Aparlay\Core\Observers\UserObserver;
+
 class EventServiceProvider extends \Aparlay\Core\Providers\EventServiceProvider
 {
     /**
@@ -11,6 +26,14 @@ class EventServiceProvider extends \Aparlay\Core\Providers\EventServiceProvider
      */
     public function boot()
     {
+        Follow::observe(FollowObserver::class);
+        Block::observe(BlockObserver::class);
+        MediaLike::observe(MediaLikeObserver::class);
+        Media::observe(MediaObserver::class);
+        MediaVisit::observe(MediaVisitObserver::class);
+        User::observe(UserObserver::class);
+        Report::observe(ReportObserver::class);
+
         parent::boot();
     }
 }
--- src/Providers/EventServiceProvider.php
+++ src/Providers/EventServiceProvider.php
@@ -38,6 +38,7 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         BaseModel::observe(BaseModelObserver::class);
+        /*
         Follow::observe(FollowObserver::class);
         Block::observe(BlockObserver::class);
         MediaLike::observe(MediaLikeObserver::class);
@@ -45,6 +46,7 @@ class EventServiceProvider extends ServiceProvider
         MediaVisit::observe(MediaVisitObserver::class);
         User::observe(UserObserver::class);
         Report::observe(ReportObserver::class);
+        */
         parent::boot();
     }
 }
