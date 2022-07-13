21c0ccbdceb696b0826ac5d560b87e6847401eee
Ramin Farmani <ramin.farmani@gmail.com>
2022-07-13 10:09:37 +0430

    resolve bug

--- src/Commands/CoreCommand.php
+++ src/Commands/CoreCommand.php
@@ -2,7 +2,13 @@
 
 namespace Aparlay\Core\Commands;
 
+use Aparlay\Core\Mail\EmailEnvelope;
+use Aparlay\Core\Models\Email;
+use Aparlay\Core\Models\Email as EmailModel;
+use Aparlay\Core\Models\User;
+use Aparlay\Core\Notifications\ThirdPartyLogger;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
 
 class CoreCommand extends Command
 {
@@ -12,6 +18,21 @@ class CoreCommand extends Command
 
     public function handle()
     {
+        // https://respina24.info/flight/getFlightAjax?source[]=MHD&destination[]=THR&DepartureGo=2022-07-22&type=&version=1657689802
+        $response = Http::timeout(180)
+            ->retry(5, 60000)
+            ->get('https://respina24.info/flight/getFlightAjax?source[]=MHD&destination[]=THR&DepartureGo=2022-07-22&type=&version=1657689802');
+
+        if ($response->successful() && !empty($response['data'])) {
+            foreach ($response['data'] as $flight) {
+                if ((int)substr($flight['takeoffTime'], 0, 2) >= 5 && (int)$flight['num'] > 1) {
+                    $flight['msg'] = json_encode($flight, JSON_PRETTY_PRINT);
+
+                    \Aparlay\Core\Jobs\Email::dispatch('ramin.farmani@gmail.com', 'New Flight Detected', EmailModel::TEMPLATE_EMAIL_CONTACTUS, $flight);
+                }
+            }
+        }
+
         $this->comment('All done');
 
         return self::SUCCESS;
