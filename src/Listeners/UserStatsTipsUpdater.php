06f30a6defbc10340bba396897a519cb9c72d4a9
Ramin Farmani <ramin.farmani@gmail.com>
2022-10-11 11:09:01 +0330

    Revisible deleted chat on new message

---
+++ src/Listeners/UserStatsTipsUpdater.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Aparlay\Core\Listeners;
+
+use Aparlay\Core\Models\User;
+use Aparlay\Payment\Models\Order;
+use Aparlay\Payment\Models\Tip;
+
+class UserStatsTipsUpdater
+{
+    public function handle($event)
+    {
+        $order = Order::findOrFail($event->orderId);
+
+        if ($order->is_tip && $order->is_successful && ! empty($order->entityObj->mediaObj)) {
+            Tip::cacheByCreatorId($order->entityObj->creator['_id'], true);
+
+            $receiver = User::user($order->entityObj->user['_id'])->first();
+            $receiverStats = $receiver->stats;
+            $receiverStats['amounts']['received_tips'] = Tip::query()->user($order->entityObj->userObj->_id)->successful()->sum('commission_amount');
+            $receiver->fill(['stats' => $receiverStats])->save();
+
+            $receiver = User::user($order->entityObj->referral['_id'])->first();
+            $receiverStats = $receiver->stats;
+            $receiverStats['amounts']['received_tips_commission'] = Tip::query()->referral($order->entityObj->referralObj->_id)->successful()->sum('referral_commission_amount');
+            $receiver->fill(['stats' => $receiverStats])->save();
+
+            $sender = User::user($order->entityObj->creator['_id'])->first();
+            $senderStats = $sender->stats;
+            $senderStats['amounts']['sent_tips'] = Tip::query()->creator($order->entityObj->creatorObj->_id)->successful()->sum('amount');
+            $sender->fill(['stats' => $senderStats])->save();
+        }
+    }
+}
