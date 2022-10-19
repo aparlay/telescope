8dc67c866f41a2d7adcf6abdfb1468d03f10b67e
Ramin Farmani <ramin.farmani@gmail.com>
2022-10-19 14:58:38 +0330

    Fix reprocess video

--- resources/views/admin/layouts/media_view_header.blade.php
+++ resources/views/admin/layouts/media_view_header.blade.php
@@ -12,7 +12,7 @@
                             <i class="fas fa-minus-circle"></i>
                             Alert
                         </button>
-                        <div class="dropdown btn-group col-md-1 mr-2 show">
+                        <div class="dropdown btn-group col-md-2 show">
                             <button class="btn btn-info btn-sm dropdown-toggle" type="button" id="dropdownMenuScore" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fas fa-cloud-download-alt"></i>
                                 <strong>Promote</strong>
@@ -24,7 +24,7 @@
                                 @endforeach
                             </div>
                         </div>
-                        <div class="dropdown btn-group col-md-1 mr-2 show">
+                        <div class="dropdown btn-group col-md-2 mr-2 show">
                             <button class="btn btn-info btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fas fa-cloud-download-alt"></i>
                                 <strong>Download</strong>
