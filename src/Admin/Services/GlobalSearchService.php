3eff0619df9f9d94f63183e351aa40c764a4dc51
Qemal Alliu <qemal.alliu@yahoo.com>
2022-10-26 05:09:22 +0200

    - implemented livewire component for searchbar - implemented service for searching - implemented user search

---
+++ src/Admin/Services/GlobalSearchService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Aparlay\Core\Admin\Services;
+
+use Aparlay\Core\Admin\Models\User;
+
+class GlobalSearchService
+{
+    public static function search(string $query): array
+    {
+        return self::searchUsers($query)->take(7)->toArray();
+    }
+
+    private static function searchUsers(string $searchQuery)
+    {
+        return User::query()
+            ->textSearch($searchQuery)
+            ->get()
+            ->map(function (User $user) {
+                return [
+                    'category' => 'User',
+                    'link' => $user->admin_url,
+                    'title' => $user->full_name
+                ];
+            });
+    }
+}
