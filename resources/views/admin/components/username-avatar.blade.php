8041eeaccab920aa362395a66531d45b8eba95c5
Ramin Farmani <ramin.farmani@gmail.com>
2022-10-27 13:18:55 +0330

    Fix bug

--- resources/views/admin/components/username-avatar.blade.php
+++ resources/views/admin/components/username-avatar.blade.php
@@ -6,7 +6,7 @@
     <div class="x-username-avatar float-left {{$class}}" @class(['mr-2' => empty($class)])>
         <a href="{{$user->admin_url}}"
            title="{{$user->username}} [{{$user->is_online ? 'online' : 'offline'}}] [{{User::getVerificationStatuses()[$user->verification_status] ?? 'none'}}]">
-            <img src="{{ $user->avatar }}?aspect_ratio=1:1&width=150" alt="" class="img-circle img-size-50 mr-2">
+            <img src="{{ $user->avatar }}?aspect_ratio=1:1&width=150" alt="" class="img-circle img-size-32 mr-2">
             {{ Str::limit($user->username, 6) }}
             <i title="{{$user->is_online ? 'online' : 'offline'}}" @class(['fa-user', 'ml-1', 'fas text-success' => $user->is_online, 'far text-gray' => !$user->is_online])></i>
             @if ($user->is_verified)
