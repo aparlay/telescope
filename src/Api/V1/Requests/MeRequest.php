960e42e7c23a2f7b2cdbefbbbe1aa168f897c6e4
Ramin Farmani <ramin.farmani@gmail.com>
2022-11-04 11:38:52 +0330

    Fix bug

--- src/Api/V1/Requests/MeRequest.php
+++ src/Api/V1/Requests/MeRequest.php
@@ -69,6 +69,9 @@ class MeRequest extends FormRequest
                 'avatar' => 'You can upload only one image file.',
             ]);
         }
+        if (empty($this->interested_in)) {
+            $this->interested_in = [UserInterestedIn::FEMALE->value];
+        }
         if (! is_array($this->interested_in) || empty($this->interested_in)) {
             throw ValidationException::withMessages([
                 'interested_in' => 'interested_in must be an array of interested choices.',
