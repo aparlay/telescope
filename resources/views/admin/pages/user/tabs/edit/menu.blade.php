0abcacf6eea585a6a2197657867d34120384d4be
István Tar <spam@devblog.hu>
2022-11-08 12:50:34 +0100

    Clean up admin user profile

--- resources/views/admin/pages/user/tabs/edit/menu.blade.php
+++ resources/views/admin/pages/user/tabs/edit/menu.blade.php
@@ -7,53 +7,75 @@
 </div>
 
 <div class="row card card-default list-group">
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Chat As Support</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Log-In As User</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Gallery</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Adjustments</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>View Chats (1)</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Un-Hide Chats</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Flush Unread Messages</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Set Password</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Logs</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Chat As Support</a>
+    <a href="{{ route('core.admin.user.login_as_user', ['user' => $user->_id]) }}" target="_blank" class="list-group-item list-group-item-action p-2">
+        <i class="fas fa-circle mr-1 text-blue"></i>Log-In As User
+    </a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Gallery</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Adjustments</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>View Chats (1)</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Un-Hide Chats</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Flush Unread Messages</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Set Password</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Logs</a>
 </div>
 
 <div class="row card card-default list-group">
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Give Warning</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Give Final Warning</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Freeze</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Suspend</a>
+    @if(config('app.enabled-features.user.warning-message'))
+        <a href="#" class="list-group-item list-group-item-action p-2" data-toggle="modal" data-target="#alertModal">
+            <i class="fas fa-circle mr-1 text-blue"></i>Give Warning
+        </a>
+    @endif
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Give Final Warning</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Freeze</a>
+    @if($user->status == \Aparlay\Core\Models\Enums\UserStatus::SUSPENDED->value)
+        <a href="#" class="list-group-item list-group-item-action p-2" data-toggle="modal" data-target="#activateModal">
+            <i class="fas fa-circle mr-1 text-blue"></i>Reactivate
+        </a>
+    @else
+        <a href="#" class="list-group-item list-group-item-action p-2" data-toggle="modal" data-target="#suspendModal">
+            <i class="fas fa-circle mr-1 text-blue"></i>Suspend
+        </a>
+    @endif
     <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Ban Send Photo</a>
     <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Ban Intro</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Ban Auto Payouts</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Ban Payouts</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Enable 14 Day Payout Freeze</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Ban Auto Payouts</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Ban Payouts</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Enable 14 Day Payout Freeze</a>
     <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Region Blacklist</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Make Invisible</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Allow Screenshots</a>
-    <a href="#" class="list-group-item list-group-item-action p-2 text-red"><i class="fas fa-circle mr-1 text-red"></i>Delete Account</a>
-    <a href="#" class="list-group-item list-group-item-action p-2 text-red"><i class="fas fa-circle mr-1 text-red"></i>Delete Account w/ Timer</a>
-    <a href="#" class="list-group-item list-group-item-action p-2 text-red"><i class="fas fa-circle mr-1 text-red"></i>Ban Account</a>
-    <a href="#" class="list-group-item list-group-item-action p-2 text-red"><i class="fas fa-circle mr-1 text-red"></i>Ban Device Fingerprint</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Make Invisible</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Allow Screenshots</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 text-red disabled"><i class="fas fa-circle mr-1 text-red"></i>Delete Account</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 text-red disabled"><i class="fas fa-circle mr-1 text-red"></i>Delete Account w/ Timer</a>
+    @if($user->status == \Aparlay\Core\Models\Enums\UserStatus::BLOCKED->value)
+        <a href="#" class="list-group-item list-group-item-action p-2 text-green" data-toggle="modal" data-target="#activateModal">
+            <i class="fas fa-circle mr-1 text-green"></i>Reactivate
+        </a>
+    @else
+        <a href="#" class="list-group-item list-group-item-action p-2 text-red" data-toggle="modal" data-target="#banModal">
+            <i class="fas fa-circle mr-1 text-red"></i>Ban Account
+        </a>
+    @endif
+    <a href="#" class="list-group-item list-group-item-action p-2 text-red disabled"><i class="fas fa-circle mr-1 text-red"></i>Ban Device Fingerprint</a>
 </div>
 
 <h5 class="row text-gray text-uppercase">Model</h5>
 <div class="row card card-default list-group">
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Reset New Status</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Reset New Status</a>
     <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Enable Red Visibility Warning</a>
     <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Ban new Chats with Models</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Ledger</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Ledger Export</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Request Payout</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Ledger</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Ledger Export</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Request Payout</a>
 </div>
 
 <h5 class="row text-gray text-uppercase">Fraud</h5>
 <div class="row card card-default list-group">
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Ban All CC Payments</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Block Un-Verified CCs</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Send CC Verification Request</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>CC Whitelist</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>3DS</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Ban All CC Payments</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Block Un-Verified CCs</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Send CC Verification Request</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>CC Whitelist</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>3DS</a>
 </div>
 
 <h5 class="row text-gray text-uppercase">Admin Tools</h5>
@@ -61,5 +83,5 @@
     <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Reviewer Account</a>
     <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Role</a>
     <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Is Staff Mo</a>
-    <a href="#" class="list-group-item list-group-item-action p-2"><i class="fas fa-circle mr-1 text-blue"></i>Debug Enabled</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Debug Enabled</a>
 </div>
--- resources/views/admin/pages/user/tabs/edit/user-info.blade.php
+++ resources/views/admin/pages/user/tabs/edit/user-info.blade.php
@@ -4,7 +4,7 @@
 @endphp
 
 <div class="card">
-    <form action="{{ route('core.admin.user.updateInfo', ['user' => $user->id]) }}" class="form-horizontal" method="post">
+    <form action="{{ route('core.admin.user.updateInfo', ['user' => $user->_id]) }}" class="form-horizontal" method="post">
         @csrf()
         @method('PUT')
         <div class="card-header">
@@ -25,7 +25,7 @@
                 <div class="form-group row">
                     <label for="id" class="col-sm-2 col-form-label">User ID</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="id" name="id" disabled="disabled" readonly="readonly" value="{{ $user->id }}">
+                        <input type="text" class="form-control" id="id" name="id" disabled="disabled" readonly="readonly" value="{{ $user->_id }}">
                     </div>
                 </div>
                 <div class="form-group row">
