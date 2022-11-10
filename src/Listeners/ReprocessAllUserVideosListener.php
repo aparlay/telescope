d25d2e76a195eff7766053d73fefabddae80fb93
Ramin Farmani <ramin.farmani@gmail.com>
2022-11-10 15:18:20 +0330

    Adding email type

--- src/Listeners/ReprocessAllUserVideosListener.php
+++ src/Listeners/ReprocessAllUserVideosListener.php
@@ -2,24 +2,16 @@
 
 namespace Aparlay\Core\Listeners;
 
+use Aparlay\Core\Events\AvatarChangedEvent;
 use Aparlay\Core\Events\UsernameChangedEvent;
-use Aparlay\Core\Helpers\Cdn;
 use Aparlay\Core\Jobs\ReprocessMedia;
-use Aparlay\Core\Models\Block;
 use Aparlay\Core\Models\Enums\MediaStatus;
-use Aparlay\Core\Models\Follow;
 use Aparlay\Core\Models\Media;
-use Aparlay\Core\Models\MediaComment;
-use Aparlay\Core\Models\MediaCommentLike;
-use Aparlay\Core\Models\MediaLike;
-use Aparlay\Core\Models\User;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Redis;
 
 class ReprocessAllUserVideosListener implements ShouldQueue
 {
-    public function handle(UsernameChangedEvent $event)
+    public function handle(UsernameChangedEvent|AvatarChangedEvent $event)
     {
         $i = 1;
         $medias = Media::creator($event->user->_id)
