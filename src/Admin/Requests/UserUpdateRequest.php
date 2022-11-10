bbad5a4a8645c2f4d6c4b25ebc6539a07cd1bc4e
farmani <farmani@users.noreply.github.com>
2022-11-10 12:37:08 +0000

    Fix styling

--- src/Admin/Requests/UserUpdateRequest.php
+++ src/Admin/Requests/UserUpdateRequest.php
@@ -43,7 +43,7 @@ class UserUpdateRequest extends FormRequest
                 Rule::unique('users', 'username')->ignore($this->user->_id, '_id'),
                 'min:3',
                 'max:30',
-                'alpha_dash'
+                'alpha_dash',
             ],
             'verification_status' => [
                 'nullable', Rule::in(UserVerificationStatus::getAllValues()),
