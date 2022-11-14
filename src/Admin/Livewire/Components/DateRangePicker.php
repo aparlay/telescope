65a4743dd12f6fc726855ed15cd5f2d58ea19f9b
Ramin Farmani <ramin.farmani@gmail.com>
2022-11-14 15:12:42 +0330

    Add export to date picker

--- src/Admin/Livewire/Components/DateRangePicker.php
+++ src/Admin/Livewire/Components/DateRangePicker.php
@@ -62,6 +62,11 @@ final class DateRangePicker extends Component
         $this->showAllDates = ! ($this->showAllDates);
     }
 
+    public function exportExcel()
+    {
+        $this->emit('export-excel');
+    }
+
     private function getSelectedPeriod(array $value)
     {
         if ($value == $this->today) {
