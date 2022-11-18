a0f545a991b8c77d3fc89d5703a28383958bb935
Ramin Farmani <ramin.farmani@gmail.com>
2022-11-18 13:52:09 +0330

    Adding unique visitors and active users

---
+++ src/Jobs/MediaBatchWatched.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Aparlay\Core\Jobs;
+
+use Aparlay\Core\Api\V1\Services\MediaService;
+use Aparlay\Core\Models\Media;
+use Aparlay\Core\Models\User;
+use Aparlay\Core\Notifications\JobFailed;
+use Exception;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use MongoDB\BSON\ObjectId;
+use Throwable;
+
+class MediaBatchWatched implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * The number of times the job may be attempted.
+     */
+    public int $tries = 10;
+
+    /**
+     * The maximum number of unhandled exceptions to allow before failing.
+     */
+    public int $maxExceptions = 3;
+
+    /**
+     * The number of seconds to wait before retrying the job.
+     *
+     * @var int|array
+     */
+    public $backoff = [5, 10, 15];
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     *
+     * @throws Exception
+     */
+    public function __construct(public array|ObjectId $medias, public string|null $userId = null)
+    {
+        $this->onQueue('low');
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $mediaService = app()->make(MediaService::class);
+        $durations = $mediaIds = [];
+        foreach ($this->medias as $item) {
+            $durations[$item['media_id']] = $item['duration'];
+            $mediaIds[] = new ObjectId($item['media_id']);
+        }
+
+        foreach (Media::query()->whereIn('_id', $mediaIds)->get() as $media) {
+            $mediaService->watched($media, $durations[(string)$media->_id]);
+        }
+    }
+
+    public function failed(Throwable $exception): void
+    {
+        if (($user = User::admin()->first()) !== null) {
+            $user->notify(new JobFailed(self::class, $this->attempts(), $exception->getMessage()));
+        }
+    }
+}
