ba1ed7807ec1185e1aa6aa65b26ef07353e80f9f
farmani <farmani@users.noreply.github.com>
2022-11-21 05:19:26 +0000

    Fix styling

--- database/migrations/2022_11_21_051159_user_setting_default_value.php
+++ database/migrations/2022_11_21_051159_user_setting_default_value.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class() extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,7 +13,6 @@ return new class extends Migration
      */
     public function up()
     {
-
         $default = [
             'otp' => false,
             'show_adult_content' => false,
