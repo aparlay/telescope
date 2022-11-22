fe85a4e196cb6a40a461dab733558a7c79fb6d5f
farmani <farmani@users.noreply.github.com>
2022-11-22 08:30:58 +0000

    Fix styling

--- src/Listeners/QueueBusyListener.php
+++ src/Listeners/QueueBusyListener.php
@@ -10,8 +10,7 @@ class QueueBusyListener
     public function handle($event)
     {
         if (($user = User::admin()->first()) !== null) {
-
-            $message = '_*Attention:*_ Queue ' . $event->queue;
+            $message = '_*Attention:*_ Queue '.$event->queue;
             $message .= ' is too busy please check horizon or make sure you have enough workers.';
             $message .= PHP_EOL.'_*Connections:*_ '.$event->connection;
             $message .= PHP_EOL.'_*Queue:*_ '.$event->queue;
