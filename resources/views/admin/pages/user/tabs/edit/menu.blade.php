60a3b0538d7a5a2c02091a71ca07e7abaaa619da
István Tar <spam@devblog.hu>
2022-11-24 10:09:03 +0100

    Change moderation menu

--- resources/views/admin/pages/user/tabs/edit/menu.blade.php
+++ resources/views/admin/pages/user/tabs/edit/menu.blade.php
@@ -11,12 +11,12 @@
     <a href="{{ route('core.admin.user.login_as_user', ['user' => $user->_id]) }}" target="_blank" class="list-group-item list-group-item-action p-2">
         <i class="fas fa-circle mr-1 text-blue"></i>Log-In As User
     </a>
-    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Gallery</a>
-    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Adjustments</a>
-    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>View Chats (1)</a>
-    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Un-Hide Chats</a>
-    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Flush Unread Messages</a>
     <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Set Password</a>
+    <!--<a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Gallery</a>-->
+    <!--<a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Adjustments</a>-->
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>View Chats (n)</a>
+    <!--<a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Un-Hide Chats</a>-->
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Flush Unread Messages</a>
     <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Logs</a>
 </div>
 
@@ -26,8 +26,10 @@
             <i class="fas fa-circle mr-1 text-blue"></i>Give Warning
         </a>
     @endif
-    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Give Final Warning</a>
-    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Freeze</a>
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Give Warning</a>
+    <!--<a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Give Final Warning</a>-->
+    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Validation Freeze</a>
+    <!--<a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Freeze</a>-->
     @if($user->status == \Aparlay\Core\Models\Enums\UserStatus::SUSPENDED->value)
         <a href="#" class="list-group-item list-group-item-action p-2" data-toggle="modal" data-target="#activateModal">
             <i class="fas fa-circle mr-1 text-blue"></i>Reactivate
@@ -37,14 +39,14 @@
             <i class="fas fa-circle mr-1 text-blue"></i>Suspend
         </a>
     @endif
-    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Ban Send Photo</a>
-    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Ban Intro</a>
+    <!--<a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Ban Send Photo</a>-->
+    <!--<a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Ban Intro</a>-->
     <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Ban Auto Payouts</a>
     <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Ban Payouts</a>
-    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Enable 14 Day Payout Freeze</a>
-    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Region Blacklist</a>
+    <!--<a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Enable 14 Day Payout Freeze</a>-->
+    <!--<a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Region Blacklist</a>-->
     <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Make Invisible</a>
-    <a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Allow Screenshots</a>
+    <!--<a href="#" class="list-group-item list-group-item-action p-2 disabled"><i class="fas fa-circle mr-1 text-blue"></i>Allow Screenshots</a>-->
     <a href="#" class="list-group-item list-group-item-action p-2 text-red disabled"><i class="fas fa-circle mr-1 text-red"></i>Delete Account</a>
     <a href="#" class="list-group-item list-group-item-action p-2 text-red disabled"><i class="fas fa-circle mr-1 text-red"></i>Delete Account w/ Timer</a>
     @if($user->status == \Aparlay\Core\Models\Enums\UserStatus::BLOCKED->value)
