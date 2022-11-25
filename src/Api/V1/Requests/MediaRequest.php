ebad73ba6bd83d8a6e019f9362d10bdbaa53a795
farmani <farmani@users.noreply.github.com>
2022-11-25 08:18:08 +0000

    Fix styling

--- src/Api/V1/Requests/MediaRequest.php
+++ src/Api/V1/Requests/MediaRequest.php
@@ -16,12 +16,13 @@ class MediaRequest extends FormRequest
     private array $mimeTypes = [
         'video/x-flv',          // Flash            .flv
         'video/mp4',            // MPEG-4           .mp4
-        'application/x-mpegURL',// iPhone Segment   .ts
+        'application/x-mpegURL', // iPhone Segment   .ts
         'video/3gpp',           // 3GP Mobile       .3gp
         'video/quicktime',      // QuickTime        .mov
         'video/x-msvideo',      // A/V Interleave   .avi
         'video/x-ms-wmv',       // Windows Media    .wmv
     ];
+
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -51,10 +52,10 @@ class MediaRequest extends FormRequest
      */
     public function prepareForValidation()
     {
-        if (!Storage::disk('upload')->exists($this->file)) {
+        if (! Storage::disk('upload')->exists($this->file)) {
             throw ValidationException::withMessages(['file' => 'Uploaded file does not exists.']);
         }
-        if (!in_array(Storage::disk('upload')->mimeType($this->file), $this->mimeTypes)) {
+        if (! in_array(Storage::disk('upload')->mimeType($this->file), $this->mimeTypes)) {
             throw ValidationException::withMessages(['file' => 'Uploaded file must be a standard video file.']);
         }
     }
