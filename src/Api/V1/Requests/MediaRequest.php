814216885a2e10323949f0798ece3e366dac6966
Ramin Farmani <ramin.farmani@gmail.com>
2022-11-25 12:02:24 +0330

    Filter possible mimetypes

--- src/Api/V1/Requests/MediaRequest.php
+++ src/Api/V1/Requests/MediaRequest.php
@@ -3,10 +3,8 @@
 namespace Aparlay\Core\Api\V1\Requests;
 
 use Aparlay\Core\Api\V1\Rules\UploadedFileExists;
-use Aparlay\Core\Api\V1\Rules\UploadedFileIsMedia;
+use Aparlay\Core\Api\V1\Rules\UploadedFileIsVideo;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Validation\ValidationException;
 
 /**
  * @property string $description
@@ -15,16 +13,6 @@ use Illuminate\Validation\ValidationException;
  */
 class MediaRequest extends FormRequest
 {
-    private array $mimeTypes = [
-        'video/x-flv',          // Flash            .flv
-        'video/mp4',            // MPEG-4           .mp4
-        'application/x-mpegURL', // iPhone Segment   .ts
-        'video/3gpp',           // 3GP Mobile       .3gp
-        'video/quicktime',      // QuickTime        .mov
-        'video/x-msvideo',      // A/V Interleave   .avi
-        'video/x-ms-wmv',       // Windows Media    .wmv
-    ];
-
     /**
      * Determine if the user is authorized to make this request.
      *
