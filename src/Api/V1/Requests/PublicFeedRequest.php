a00a0ed86118f92af7ca6f97e5d32979530df347
Ramin Farmani <ramin.farmani@gmail.com>
2022-11-28 14:21:17 +0330

    Fix bug

--- src/Api/V1/Requests/PublicFeedRequest.php
+++ src/Api/V1/Requests/PublicFeedRequest.php
@@ -73,7 +73,7 @@ class PublicFeedRequest extends FormRequest
             UserSettingShowAdultContent::ALL->label() => UserSettingShowAdultContent::ALL->value,
             default => null
         };
-        $showAdultContent = $showAdultContent ?? (auth()->guest() ? auth()->user()->setting['show_adult_content'] : 1);
+        $showAdultContent = $showAdultContent ?? (auth()->guest() ? 1 : auth()->user()->setting['show_adult_content']);
 
         $this->merge([
             'uuid' => request()->cookie('__Secure_uuid', request()->header('X-DEVICE-ID', '')),
