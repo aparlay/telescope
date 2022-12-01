9340ae05ed1e66519e3b0adffc652abb4d564d2d
Ramin Farmani <ramin.farmani@gmail.com>
2022-12-01 15:01:18 +0330

    Fix bug

--- resources/views/admin/pages/media/view.blade.php
+++ resources/views/admin/pages/media/view.blade.php
@@ -236,7 +236,10 @@
                                             <label for="feature_demo" class="col-sm-2 col-form-label">Private Video</label>
                                             <div class="col-sm-10">
                                                 <div class="custom-control custom-switch mt-2">
-                                                    <input type="checkbox" value="0" class="custom-control-input" name="visibility" id="visibility" {!! ($media->visibility == 0) ? 'checked' : '' !!}>
+                                                    <input type="hidden" name="visibility" value="1">
+                                                    <input type="checkbox" value="0" class="custom-control-input"
+                                                           name="visibility"
+                                                           id="visibility" {!! ($media->visibility == 0) ? 'checked' : '' !!}>
                                                     <label class="custom-control-label" for="visibility"></label>
                                                 </div>
                                             </div>
