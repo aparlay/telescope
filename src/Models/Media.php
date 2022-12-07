4541b2580779ce78de270da7e7d1f44217ca6df3
Ramin Farmani <ramin.farmani@gmail.com>
2022-12-07 14:53:03 +0330

    Adding skin score to media resource

--- src/Models/Media.php
+++ src/Models/Media.php
@@ -573,7 +573,9 @@ class Media extends BaseModel
      */
     public function getContentGenderLabelAttribute(): string
     {
-        return $this->content_gender ? MediaContentGender::from($this->content_gender)->label() : '';
+        return in_array($this->content_gender, MediaContentGender::getAllValues()) ?
+            MediaContentGender::from($this->content_gender)->label() :
+            '';
     }
 
     /**
