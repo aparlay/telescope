c59c207a5b70f76548d5d281c199930eb66be729
Ramin Farmani <ramin.farmani@gmail.com>
2022-12-09 11:15:52 +0330

    Adding returned user new score system

--- resources/views/livewire/medias-table.blade.php
+++ resources/views/livewire/medias-table.blade.php
@@ -18,7 +18,8 @@
                     'btn-outline-info' => !in_array(Arr::get($sort, 'sort_scores.guest'), [1, -1])])
                             wire:model="sort.sort_scores.guest"
                             wire:click="sort('sort_scores.guest', -1)">
-                        Ordered By New Guest {{ Arr::get($sort, 'sort_scores.guest') == 1 ? '↑' : ''}}{{ Arr::get($sort, 'sort_scores.guest') == -1 ? '↓' : ''}}
+                        Ordered By New
+                        Guest {{ Arr::get($sort, 'sort_scores.guest') == 1 ? '↑' : ''}}{{ Arr::get($sort, 'sort_scores.guest') == -1 ? '↓' : ''}}
                     </button>
                     <button @class([
                         'btn btn-sm',
@@ -26,7 +27,8 @@
                         'btn-outline-dark' => !in_array(Arr::get($sort, 'sort_scores.returned'), [1, -1])])
                             wire:model="sort.sort_scores.returned"
                             wire:click="sort('sort_scores.returned', -1)">
-                        Ordered By Returned {{ Arr::get($sort, 'sort_scores.returned') == 1 ? '↑' : ''}}{{ Arr::get($sort, 'sort_scores.returned') == -1 ? '↓' : ''}}
+                        Ordered By
+                        Returned {{ Arr::get($sort, 'sort_scores.returned') == 1 ? '↑' : ''}}{{ Arr::get($sort, 'sort_scores.returned') == -1 ? '↓' : ''}}
                     </button>
                     <button @class([
                         'btn btn-sm',
@@ -34,7 +36,8 @@
                         'btn-outline-primary' => !in_array(Arr::get($sort, 'sort_scores.registered'), [1, -1])])
                             wire:model="sort.sort_scores.registered"
                             wire:click="sort('sort_scores.registered', -1)">
-                        Ordered By Login {{ Arr::get($sort, 'sort_scores.registered') == 1 ? '↑' : ''}}{{ Arr::get($sort, 'sort_scores.registered') == -1 ? '↓' : ''}}
+                        Ordered By
+                        Login {{ Arr::get($sort, 'sort_scores.registered') == 1 ? '↑' : ''}}{{ Arr::get($sort, 'sort_scores.registered') == -1 ? '↓' : ''}}
                     </button>
                 </h4>
             </div>
@@ -65,14 +68,14 @@
 
     <table class="table table-striped">
         <tbody>
-            <tr>
-                <th class="col-md-1">
-                    <x-sortable-column-header :sort="$sort" :fieldName="'file'" :fieldLabel="'Cover'"/>
-                </th>
+        <tr>
+            <th class="col-md-2">
+                <x-sortable-column-header :sort="$sort" :fieldName="'file'" :fieldLabel="'Cover'"/>
+            </th>
 
             <th @class(['col-md-3', 'd-none' => $hiddenFields['creator_username']])>
-                    <x-sortable-column-header :sort="$sort" :fieldName="'creator.username'" :fieldLabel="'Creator'"/>
-                </th>
+                <x-sortable-column-header :sort="$sort" :fieldName="'creator.username'" :fieldLabel="'Creator'"/>
+            </th>
 
             <td class="col-md-1">
                 <div>
@@ -90,7 +93,7 @@
                     <label for="">Stats</label>
                 </div>
             </td>
-            <td class="col-md-1">
+            <td class="col-md-2">
                 <div>
                     <label for="">Score</label>
                 </div>
@@ -100,63 +103,65 @@
                     <x-sortable-column-header :sort="$sort" :fieldName="'created_at'" :fieldLabel="'Created At'"/>
                 </div>
             </td>
-            <td class="col-md-1">
-                <div>
-                    <label for="">Details</label>
-                </div>
-            </td>
 
-                <th class="col-md-2 text-right">
-                    <label for="">Actions</label>
-                </th>
-            </tr>
+            <th class="col-md-1 text-right">
+                <label for="">Actions</label>
+            </th>
+        </tr>
 
-            @foreach($medias as $media)
-                <tr>
-                    <td>
-                        <x-media-cover :media="$media"/>
-                    </td>
+        @foreach($medias as $media)
+            <tr>
+                <td>
+                    <x-media-cover :media="$media"/>
+                </td>
 
-                    <td @class(['col-md-2', 'd-none' => $hiddenFields['creator_username']])>
-                        <x-username-avatar :user="$media->creatorObj"/>
-                    </td>
+                <td @class(['d-none' => $hiddenFields['creator_username']])>
+                    <x-username-avatar :user="$media->creatorObj"/>
+                </td>
 
-                    <td class="col-md-1">
+                <td>
                         <span class="badge bg-{{ MediaStatus::from($media->status)->badgeColor() }}">
                             {{ MediaStatus::from($media->status)->label() }}
                         </span>
-                    </td>
+                </td>
 
-                    <td class="col-md-1">
-                       <i class="fa fa-heart fa-fw text-danger" title="Number of Likes"></i> {{$media->like_count}} <br>
+                <td>
+                    <i class="fa fa-heart fa-fw text-danger" title="Number of Likes"></i> {{$media->like_count}} <br>
                     <i class="fa fa-eye fa-fw text-primary" title="Number of Visits"></i> {{$media->visit_count}} <br>
                     <i class="fa fa-play fa-fw text-primary" title="Number of Play"></i> {{$media->watched_count}} <br>
-                    <i class="fa fa-clock fa-fw text-primary" title="Watch Duration"></i> {{ round(\Carbon\CarbonInterval::seconds($media->length_watched)->cascade()->totalHours) }}H. <br>
-                    <i class="fa fa-comments fa-fw text-warning" title="Number of Comments"></i> {{$media->comment_count}} <br>
+                    <i class="fa fa-clock fa-fw text-primary"
+                       title="Watch Duration"></i> {{ round(\Carbon\CarbonInterval::seconds($media->length_watched)->cascade()->totalHours) }}
+                    H. <br>
+                    <i class="fa fa-comments fa-fw text-warning"
+                       title="Number of Comments"></i> {{$media->comment_count}} <br>
                     <i class="fa fa-money-bill-wave fa-fw text-info" title="Total Tips"></i> {{(int)$media->tips/100}}
-                    </td>
+                </td>
 
-                    <td class="col-md-1">
-                       <i class="fa fa-user-shield fa-fw text-primary" title="Score for Registered"></i> {{$media->sort_scores['registered']}} <br>
-                    <i class="fa fa-user-minus fa-fw text-info" title="Score for New Guest"></i> {{$media->sort_scores['guest']}} <br>
-                    <i class="fa fa-user-minus fa-fw text-dark" title="Score for Returned Guest"></i> {{$media->sort_scores['returned']}} <br>
+                <td>
+                    <i class="fa fa-user-shield fa-fw text-primary"
+                       title="Score for Registered"></i> {{$media->sort_scores['registered']}} <br>
+                    <i class="fa fa-user-minus fa-fw text-info"
+                       title="Score for New Guest"></i> {{$media->sort_scores['guest']}} <br>
+                    <i class="fa fa-user-minus fa-fw text-dark"
+                       title="Score for Returned Guest"></i> {{$media->sort_scores['returned']}} <br>
                     <hr class="my-1">
                     <div class="text-sm-left">
-                    @foreach($media->scores as $score)
-                        {{$score['type']}}: {{$score['score']}} <br>
-                    @endforeach
+                        @foreach($media->scores as $score)
+                            {{$score['type']}}: {{$score['score']}} <br>
+                        @endforeach
                     </div>
-                    </td>
+                </td>
 
-                    <td class="col-md-1">
-                        {{$media->created_at}}
-                    </td>
+                <td>
+                    {{$media->created_at}}
+                </td>
 
-                    <td class="col-md-1 text-right">
-                        <a class="btn btn-primary btn-sm" href="{{$media->admin_url}}" title="View"><i class="fas fa-eye"></i></a>
-                    </td>
-                </tr>
-            @endforeach
+                <td class="text-right">
+                    <a class="btn btn-primary btn-sm" href="{{$media->admin_url}}" title="View"><i
+                                class="fas fa-eye"></i></a>
+                </td>
+            </tr>
+        @endforeach
         </tbody>
     </table>
     {{ $medias->links() }}
