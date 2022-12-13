daddf1c08082b5c4c7e3206ae5e5f01c17768493
Ramin Farmani <ramin.farmani@gmail.com>
2022-12-13 14:23:37 +0330

    Adding account approvement notification

--- src/Admin/Livewire/Modals/UserVerificationModal.php
+++ src/Admin/Livewire/Modals/UserVerificationModal.php
@@ -166,13 +166,15 @@ class UserVerificationModal extends Component
             $payload['verification_status_label'] = UserVerificationStatus::from($newVerificationStatus)->label();
 
 
-            $email['subject'] = 'You have been verified!';
-            $email['content'] = 'Your Creator application has been reject! 😔';
+            $email['subject'] = 'Oops there was a problem with your verification';
+            $email['title'] = 'Verification Rejected';
+            $email['body'] = 'Unfortunately there was a problem with your ID Verification. Please go back to ‘Request verification’ in the settings to see how to resolve this.';
             $email['isVerified'] = false;
             if ((int)$newVerificationStatus === UserVerificationStatus::VERIFIED->value) {
+                $email['subject'] = 'Congratulations! Your account has been verified!';
+                $email['title'] = 'Account verified!';
+                $email['body'] = 'Congratulations your account has been verified. A pink checkmark is now visible next to your username.';
                 $email['isVerified'] = true;
-                $email['subject'] = 'You have been verified!';
-                $email['content'] = 'Congratulations you are now a verified creator!';
             }
             if (
                 in_array(
