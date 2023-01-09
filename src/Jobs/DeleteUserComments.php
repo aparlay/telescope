c61bbb2e34be9adcb80f9f8c724ec13f5a44da7c
Filip Vujnovic <vujnovicfilip@gmail.com>
2023-01-09 13:32:42 +0100

    Delete comments for blocked users

---
+++ src/Jobs/DeleteUserComments.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Aparlay\Core\Jobs;
+
+use Aparlay\Core\Models\Block;
+use Aparlay\Core\Models\Follow;
+use Aparlay\Core\Models\MediaComment;
+use Aparlay\Core\Models\User;
+use Aparlay\Core\Notifications\JobFailed;
+use Exception;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use MongoDB\BSON\ObjectId;
+use Throwable;
+
+class DeleteUserComments implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    public string $userId;
+
+    /**
+     * The number of times the job may be attempted.
+     */
+    public int $tries = 20;
+
+    /**
+     * The maximum number of unhandled exceptions to allow before failing.
+     */
+    public int $maxExceptions = 3;
+
+    /**
+     * The number of seconds to wait before retrying the job.
+     *
+     * @var int|array
+     */
+    public $backoff = 60;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     *
+     * @throws Exception
+     */
+    public function __construct(string $userId)
+    {
+        $this->onQueue('low');
+        $this->userId = $userId;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        MediaComment::query()->where('creator._id', $this->userId)->chunk(200, function ($comments) {
+            foreach ($comments as $comment) {
+                $comment->delete();
+            }
+        });
+    }
+
+    public function failed(Throwable $exception)
+    {
+        if (($user = User::admin()->first()) !== null) {
+            $user->notify(new JobFailed(self::class, $this->attempts(), $exception->getMessage()));
+        }
+    }
+}
