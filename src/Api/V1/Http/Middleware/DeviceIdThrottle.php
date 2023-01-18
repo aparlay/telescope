6ee6ad3c1f19885bd90b8cac88d8bd45cf187baf
farmani <farmani@users.noreply.github.com>
2023-01-18 18:38:58 +0000

    Fix styling

--- src/Api/V1/Http/Middleware/DeviceIdThrottle.php
+++ src/Api/V1/Http/Middleware/DeviceIdThrottle.php
@@ -13,6 +13,7 @@ class DeviceIdThrottle extends ThrottleRequests
         if ($request->header('X-DEVICE-ID') == 'stress-test-4c55-acb3-952c2ae699f3') {
             $deviceId = 'stress-test-4c55-acb3-'.random_int(1, 1000000);
             $request->headers->set('X-DEVICE-ID', $deviceId);
+
             return $deviceId;
         }
         // Throttle by a particular header
