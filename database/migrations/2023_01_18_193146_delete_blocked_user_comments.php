9ee2e18836a67223b1c05eb2439b42cc0069d715
Filip Vujnovic <vujnovicfilip@gmail.com>
2023-01-18 20:34:00 +0100

    Added migration to delete comments for existing blocked users

---
+++ database/migrations/2023_01_18_193146_delete_blocked_user_comments.php
@@ -0,0 +1,31 @@
+<?php
+
+use Aparlay\Core\Jobs\DeleteUserMediaComments;
+use Aparlay\Core\Models\Enums\UserStatus;
+use Aparlay\Core\Models\User;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach (User::where('status', UserStatus::BLOCKED->value)->lazy() as $user) {
+            DeleteUserMediaComments::dispatch((string) $user->_id);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
