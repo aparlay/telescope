0ae0419d9f24215d2a6ac569c4f85f8383a36148
Ramin Farmani <ramin.farmani@gmail.com>
2023-01-24 17:11:12 +0330

    Adding new collection for black list content

--- src/Models/Enums/BlackListType.php
+++ src/Models/Enums/BlackListType.php
@@ -6,21 +6,18 @@ enum BlackListType: string implements Enum
 {
     use EnumEnhancements;
 
-    case USER_DEVICE_ID = 'user_device_id';
     case TEMPORARY_EMAIL_SERVICE = 'temporary_email_service';
 
     public function label(): string
     {
         return match ($this) {
-            self::USER_DEVICE_ID => __('user'),
-            self::TEMPORARY_EMAIL_SERVICE => __('media removed'),
+            self::TEMPORARY_EMAIL_SERVICE => __('temporary email service'),
         };
     }
 
     public function badgeColor(): string
     {
         return match ($this) {
-            self::USER_DEVICE_ID => 'warning',
             self::TEMPORARY_EMAIL_SERVICE => 'info'
         };
     }
