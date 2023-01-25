047b0fb2a99cb2565b769a0f6b7d1e1d1b356c43
farmani <farmani@users.noreply.github.com>
2023-01-25 12:19:53 +0000

    Fix styling

--- src/Commands/WarmupCacheCommand.php
+++ src/Commands/WarmupCacheCommand.php
@@ -26,7 +26,6 @@ class WarmupCacheCommand extends Command implements Isolatable
                     $media->storeInGeneralCaches();
                 }
             });
-
         return self::SUCCESS;
     }
 }
