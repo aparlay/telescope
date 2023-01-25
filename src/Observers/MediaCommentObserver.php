41f38e99a27d6e376db5e3edd23295a560fa8de3
zetelion <vladimir@lagutin.ru>
2023-01-25 15:40:25 +0700

    Fix bug with update notification after comment deleted

--- src/Observers/MediaCommentObserver.php
+++ src/Observers/MediaCommentObserver.php
@@ -80,6 +80,11 @@ class MediaCommentObserver extends BaseModelObserver
                 ->category(UserNotificationCategory::COMMENTS->value)
                 ->mediaEntity($media->_id)
                 ->update(['status' => UserNotificationStatus::INVISIBLE->value]);
+        } else {
+            UserNotification::query()
+                ->category(UserNotificationCategory::COMMENTS->value)
+                ->mediaEntity($media->_id)
+                ->update(['message' => $media->commentsNotificationMessage()]);
         }
     }
 }
