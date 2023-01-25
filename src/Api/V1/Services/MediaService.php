227560cb6ae956c3b27e43cc9379d1af0631314e
Ramin Farmani <ramin.farmani@gmail.com>
2023-01-25 20:11:09 +0330

    Fix issue

--- src/Api/V1/Services/MediaService.php
+++ src/Api/V1/Services/MediaService.php
@@ -492,12 +492,12 @@ class MediaService
 
         $newCacheKey = $cacheKey.':'.$sortCategory;
         match ($explicitVisibility) {
-            UserSettingShowAdultContent::NEVER->value => \Redis::zdiffstore($newCacheKey, [$mediaIdsCacheKey, $toplessMediaIdsCacheKey, $cacheKey]),
-            UserSettingShowAdultContent::TOPLESS->value => \Redis::zdiffstore($newCacheKey, [$mediaIdsCacheKey, $explicitMediaIdsCacheKey, $cacheKey]),
-            default => \Redis::zdiffstore($newCacheKey, [$mediaIdsCacheKey, $cacheKey]),
+            UserSettingShowAdultContent::NEVER->value => Redis::zInterStore($newCacheKey, [$mediaIdsCacheKey, $toplessMediaIdsCacheKey, $cacheKey], [0, -1]),
+            UserSettingShowAdultContent::TOPLESS->value => Redis::zInterStore($newCacheKey, [$mediaIdsCacheKey, $explicitMediaIdsCacheKey, $cacheKey], [0, -1]),
+            default => Redis::zInterStore($newCacheKey, [$mediaIdsCacheKey, $cacheKey], [0, -1]),
         };
 
-        return \Redis::zrevrange($newCacheKey, 0, 500);
+        return Redis::zrevrange($newCacheKey, 0, 500);
     }
 
     /**
