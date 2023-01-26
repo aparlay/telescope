f2ae8555eda592caf6b2a19a99a8b960437e5d58
Ramin Farmani <ramin.farmani@gmail.com>
2023-01-26 15:27:12 +0330

    Fix issue

--- src/Api/V1/Services/MediaService.php
+++ src/Api/V1/Services/MediaService.php
@@ -485,7 +485,7 @@ class MediaService
             default => Redis::sdiff($mediaIdsCacheKey, $cacheKey),
         };
 
-        return array_slice($notVisitedIds, 0, 1000);
+        return array_slice($notVisitedIds, 0, 1500);
     }
 
     /**
