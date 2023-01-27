ceb6256b46e0efd7e6a2aa488737f5a7bb0f250e
Ramin Farmani <ramin.farmani@gmail.com>
2023-01-27 16:54:23 +0330

    Fix issue

--- src/Api/V1/Services/MediaService.php
+++ src/Api/V1/Services/MediaService.php
@@ -516,7 +516,8 @@ class MediaService
             }
 
             $cacheKey = config('database.redis.options.prefix').$notVisitedTopVideosCacheKey;
-            Redis::rawCommand('ZDIFFSTORE', $cacheKey, count($filterBuckets) + 1, $mediaIdsCacheKey, ...$filterBuckets);
+            $args = [$cacheKey, count($filterBuckets) + 1, $mediaIdsCacheKey, ...$filterBuckets];
+            Redis::rawCommand('ZDIFFSTORE',  ...$args);
             Redis::expireAt($notVisitedTopVideosCacheKey, now()->addHour()->getTimestamp());
         }
 
