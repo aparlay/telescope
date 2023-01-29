ccdd5392af0bad75ee28ebe0208798e4465f5e17
Ramin Farmani <ramin.farmani@gmail.com>
2023-01-29 09:06:54 +0330

    Fix issue

--- src/Jobs/MediaForceSortPositionRecalculate.php
+++ src/Jobs/MediaForceSortPositionRecalculate.php
@@ -63,56 +63,29 @@ class MediaForceSortPositionRecalculate implements ShouldQueue
                 ->select(['_id'])
                 ->get()
                 ->pluck(['_id'])
-                ->map(fn ($mediaId) => new ObjectId($mediaId))
                 ->toArray();
             if (empty($forcedPositionMediaIds)) {
                 continue;
             }
-
             $forcedMedias = Media::availableForFollower()
                 ->hasForceSortPosition($category)
                 ->orderBy('force_sort_positions.'.$category)
                 ->get();
-            $forcedPositionMax = $forcedMedias->last()->force_sort_positions[$category];
+            foreach ($forcedMedias as $forcedMedia) {
+                $position = $forcedMedia->force_sort_positions[$category];
+                $locatedMediaInPosition = Media::public()
+                    ->confirmed()
+                    ->sort($category) // desc
+                    ->whereNotIn('_id', collect($forcedPositionMediaIds)->map(fn ($mediaId) => new ObjectId($mediaId))->toArray())
+                    ->offset($position - 1)
+                    ->first();
+                $sortScores = $forcedMedia->sort_scores;
+                $sortScores[$category] = $locatedMediaInPosition->sort_scores[$category] + (0.0000001*count($forcedPositionMediaIds));
+                $forcedMedia->sort_scores = $sortScores;
+                $forcedMedia->save();
+                $forcedMedia->storeInGeneralCaches();
 
-            $neighborMedias = Media::public()
-                ->confirmed()
-                ->sort($category) // desc
-                ->whereNotIn('_id', $forcedPositionMediaIds)
-                ->limit($forcedPositionMax)
-                ->get();
-
-            $bottomScore = $neighborMedias->first()->sort_scores[$category];
-            $stepScore = 0.00001;
-
-            $position = 1;
-            while ($position <= $forcedPositionMax) {
-                foreach ($forcedMedias as $forcedMedia) {
-                    if ($position === (int) $forcedMedia->force_sort_positions[$category]) {
-                        $medias[$position] = $forcedMedia;
-                    }
-                }
-
-                if (! isset($medias[$position])) {
-                    $medias[$position] = $neighborMedias->shift();
-                }
-
-                $position++;
-            }
-
-            foreach ($neighborMedias->all() as $neighborMedia) {
-                $medias[$position] = $neighborMedia;
-                $position++;
-            }
-
-            $lowestScore = $bottomScore - $stepScore;
-            while ($position-- > 1) {
-                $sortScores = $medias[$position]->sort_scores;
-                $lowestScore += $stepScore;
-                $sortScores[$category] = $lowestScore;
-                $medias[$position]->sort_scores = $sortScores;
-                $medias[$position]->save();
-                $medias[$position]->storeInGeneralCaches();
+                $forcedPositionMediaIds = array_diff($forcedPositionMediaIds, [(string) $forcedMedia->_id]);
             }
         }
     }
@@ -124,3 +97,4 @@ class MediaForceSortPositionRecalculate implements ShouldQueue
         }
     }
 }
+
