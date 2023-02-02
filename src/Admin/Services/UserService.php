9c3fec33704bd19e74b6c22415c0360f20354ab7
fvfvfvfv <fvfvfvfv@users.noreply.github.com>
2023-02-02 12:10:50 +0000

    Fix styling

--- src/Admin/Services/UserService.php
+++ src/Admin/Services/UserService.php
@@ -171,7 +171,7 @@ class UserService extends AdminBaseService
 
         $dataBooleans = [
             'email_verified' => $request->boolean('email_verified'),
-            'visibility' => (int) $request->boolean('visibility')
+            'visibility' => (int) $request->boolean('visibility'),
         ];
 
         $data = array_merge($data, $dataBooleans);
