8e7e9bc2733997e44fb61b0ba86059bbbd2fd2c2
Ramin Farmani <ramin.farmani@gmail.com>
2023-02-02 12:38:23 +0330

    set payout settings

---
+++ database/migrations/2023_02_02_090651_fix_user_settings.php
@@ -0,0 +1,36 @@
+<?php
+
+use Aparlay\Core\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach (User::lazy() as $user) {
+            $setting = $user->setting;
+            $setting['payout'] = [
+                'ban_payout' => false,
+                'auto_ban_payout' => false,
+            ];
+            $user->saveQuietly();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
