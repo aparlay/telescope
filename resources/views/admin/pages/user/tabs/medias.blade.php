91dfa4f4d41d1812483fc33538dcbc22d891794d
Filip Vujnovic <vujnovicfilip@gmail.com>
2023-02-08 19:18:38 +0100

    Media tab active first

--- resources/views/admin/pages/user/tabs/medias.blade.php
+++ resources/views/admin/pages/user/tabs/medias.blade.php
@@ -3,17 +3,17 @@
         <div class="container-fluid mt-3">
             <ul class="nav nav-tabs">
                 <li class="nav-item">
-                    <a class="nav-link active" data-toggle="tab" href="#media-tab-avatar">Avatar</a>
+                    <a class="nav-link" data-toggle="tab" href="#media-tab-avatar">Avatar</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" data-toggle="tab" href="#media-tab-media">Media</a>
+                    <a class="nav-link active" data-toggle="tab" href="#media-tab-media">Media</a>
                 </li>
             </ul>
             <div class="tab-content">
-                <div class="tab-pane container active" id="media-tab-avatar">
+                <div class="tab-pane container" id="media-tab-avatar">
                     @include('default_view::admin.pages.user.tabs.media-categories.avatar', ['user' => $user])
                 </div>
-                <div class="tab-pane container" id="media-tab-media">
+                <div class="tab-pane container active" id="media-tab-media">
                     <livewire:user-media :creatorId="$user->id" />
                 </div>
             </div>
