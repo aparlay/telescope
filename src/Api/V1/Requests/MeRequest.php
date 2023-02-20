2a9b7da168a634647c4c298db914def4fe2f243d
benascbr <benascbr@users.noreply.github.com>
2023-02-20 08:10:34 +0000

    Fix styling

--- src/Api/V1/Requests/MeRequest.php
+++ src/Api/V1/Requests/MeRequest.php
@@ -89,7 +89,7 @@ class MeRequest extends FormRequest
             ]);
         }
 
-        if (!auth()->guest() && auth()->user()->is_invisible && isset($this->visibility)) {
+        if (! auth()->guest() && auth()->user()->is_invisible && isset($this->visibility)) {
             throw ValidationException::withMessages([
                 'visibility' => 'Your account is invisible by administrator, you cannot change it to public/private.',
             ]);
@@ -99,12 +99,12 @@ class MeRequest extends FormRequest
             $this->merge(['username' => trim($this->username)]);
         }
 
-        if (!auth()->guest() && auth()->user()->status === UserStatus::ACTIVE->value && $this->has('username')) {
+        if (! auth()->guest() && auth()->user()->status === UserStatus::ACTIVE->value && $this->has('username')) {
             throw ValidationException::withMessages([
                 'username' => 'You cannot change your username any more.',
             ]);
         }
-        if (!auth()->guest() && !empty(auth()->user()->payout_country_alpha2) && $this->has('payout_country_alpha2')) {
+        if (! auth()->guest() && ! empty(auth()->user()->payout_country_alpha2) && $this->has('payout_country_alpha2')) {
             throw ValidationException::withMessages([
                 'username' => 'You cannot change your payout country any more.',
             ]);
@@ -138,7 +138,7 @@ class MeRequest extends FormRequest
                     'allow_unverified_cc' => $user->setting['payment']['allow_unverified_cc'] ?? false,
                     'block_unverified_cc' => $user->setting['payment']['block_unverified_cc'] ?? true,
                     'block_cc_payments' => $user->setting['payment']['block_cc_payments'] ?? true,
-                    'unverified_cc_spent_amount' => (int)($user->setting['payment']['unverified_cc_spent_amount'] ?? 0),
+                    'unverified_cc_spent_amount' => (int) ($user->setting['payment']['unverified_cc_spent_amount'] ?? 0),
                 ],
                 'payout' => [
                     'ban_payout' => $user->setting['payout']['ban_payout'] ?? false,
