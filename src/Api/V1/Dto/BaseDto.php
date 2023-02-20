4b1130ae8ac5211b5a8c9f47af2c8f26c302c8cc
Ramin Farmani <ramin.farmani@gmail.com>
2023-02-20 10:01:47 +0330

    Editor setting file

---
+++ src/Api/V1/Dto/BaseDto.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Aparlay\Core\Api\V1\Dto;
+
+use Spatie\DataTransferObject\DataTransferObject;
+
+#[Strict]
+abstract class BaseDto extends DataTransferObject
+{
+    /**
+     * @param  array  $data
+     *
+     * @return void
+     * @throws \ErrorException
+     */
+    public function load(array $data)
+    {
+        foreach ($data as $prop => $value) {
+            if (!property_exists($this, $prop)) {
+                $className = get_class($this);
+                throw new \ErrorException("{$prop} does not exists for this DTO $className");
+            }
+            $this->{$prop} = $value;
+        }
+    }
+}
