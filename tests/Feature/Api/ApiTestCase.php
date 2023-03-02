5506c8f76fb290821304124dc60b0a9413057311
Ramin Farmani <ramin.farmani@gmail.com>
2023-03-02 17:05:02 +0330

    Enhance tests

--- tests/Feature/Api/ApiTestCase.php
+++ tests/Feature/Api/ApiTestCase.php
@@ -4,7 +4,6 @@ namespace Aparlay\Core\Tests\Feature\Api;
 
 use Aparlay\Core\Api\V1\Http\Middleware\DeviceIdThrottle;
 use Aparlay\Core\Tests\TestCase;
-use Artisan;
 
 class ApiTestCase extends TestCase
 {
