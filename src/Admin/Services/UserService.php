3761daa2cf082d6f4290d0d9cc5513725963839e
Filip Vujnovic <vujnovicfilip@gmail.com>
2023-03-07 21:15:01 +0100

    Requested changes

--- src/Admin/Services/UserService.php
+++ src/Admin/Services/UserService.php
@@ -256,14 +256,17 @@ class UserService extends AdminBaseService
             $oldFileName = $user->avatar;
             $this->userRepository->update(['avatar' => Storage::disk('public')->url('avatars/'.$avatar)], $user->_id);
 
-            if (! config('app.is_testing')) {
-                Bus::chain([
-                    new DeleteMediaMetadata('avatars/'.$avatar, 'public'),
-                    (new UploadAvatar((string) $user->_id, 'avatars/'.$avatar))->delay(10),
-                ])
-                ->onQueue(config('app.server_specific_queue'))
-                ->dispatch();
-            }
+            Bus::chain([
+                function() use ($avatar) {
+                    DeleteMediaMetadata::dispatchIf(! config('app.is_testing'), 'avatars/'.$avatar, 'public');
+                },
+                function() use ($user, $avatar) {
+                    UploadAvatar::dispatchIf(! config('app.is_testing'), (string) $user->_id, 'avatars/'.$avatar)->delay(10);
+                }
+            ])
+            ->onQueue(config('app.server_specific_queue'))
+            ->dispatch();
+
             DeleteAvatar::dispatchIf(! str_contains($oldFileName, 'default_'), basename($oldFileName))->delay(100);
         }
 
