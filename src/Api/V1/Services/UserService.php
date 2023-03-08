ae5a4e5f9356937a6257a44e278ca32443143eda
Filip Vujnovic <vujnovicfilip@gmail.com>
2023-03-08 09:38:59 +0100

    Revert requested changes

--- src/Api/V1/Services/UserService.php
+++ src/Api/V1/Services/UserService.php
@@ -95,16 +95,14 @@ class UserService
             $avatarUrl = str_replace('api.waptap.com', 'api1.waptap.com', Storage::disk('public')->url('avatars/'.$avatar));
             $this->userRepository->update(['avatar' => $avatarUrl], $user->_id);
 
-            Bus::chain([
-                function () use ($avatar) {
-                    DeleteMediaMetadata::dispatchIf(! config('app.is_testing'), 'avatars/'.$avatar, 'public');
-                },
-                function () use ($user, $avatar) {
-                    UploadAvatar::dispatchIf(! config('app.is_testing'), (string) $user->_id, 'avatars/'.$avatar)->delay(10);
-                },
-            ])
-            ->onQueue(config('app.server_specific_queue'))
-            ->dispatch();
+            if (! config('app.is_testing')) {
+                Bus::chain([
+                    new DeleteMediaMetadata('avatars/'.$avatar, 'public'),
+                    (new UploadAvatar((string) $user->_id, 'avatars/'.$avatar))->delay(10),
+                ])
+                    ->onQueue(config('app.server_specific_queue'))
+                    ->dispatch();
+            }
             DeleteAvatar::dispatchIf(! str_contains($oldFileName, 'default_'), basename($oldFileName))->delay(100);
         }
 
