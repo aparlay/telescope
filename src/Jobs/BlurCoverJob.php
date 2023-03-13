53fe5524cb4cbeffef9c2591c41fe5f3f63d10fa
Ramin Farmani <ramin.farmani@gmail.com>
2023-03-13 21:33:16 +0330

    Fix blur image

--- src/Jobs/BlurCoverJob.php
+++ src/Jobs/BlurCoverJob.php
@@ -80,7 +80,7 @@ class BlurCoverJob extends AbstractJob implements ShouldQueue
             }
 
             $blurredImage = Uuid::uuid4().'.jpg';
-            Image::load($storage->path($image))->blur(15)->quality(70)->save($storage->path($blurredImage));
+            Image::load($storage->path($image))->blur(85)->quality(70)->save($storage->path($blurredImage));
             Storage::disk(StorageType::GC_COVERS)->writeStream($blurredImage, $storage->readStream($blurredImage));
             $media->image_blurred = $blurredImage;
             $media->saveQuietly();
