5ff1567409214b414c3ba6d6ddbc0ec18cb650bf
Ramin Farmani <ramin.farmani@gmail.com>
2023-03-24 16:52:26 +0330

    Change Health check endpoint

--- src/Api/V1/Controllers/HealthCheckJsonResultsController.php
+++ src/Api/V1/Controllers/HealthCheckJsonResultsController.php
@@ -11,6 +11,12 @@ use Spatie\Health\ResultStores\StoredCheckResults\StoredCheckResult;
 
 class HealthCheckJsonResultsController extends Controller
 {
+    protected const IGNORED_CHECKS = [
+        'UsedDiskSpace',
+        'Environment',
+        'DebugMode'
+    ];
+
     public function __invoke(Request $request, ResultStore $resultStore): Response
     {
         if ($request->has('fresh') || config('health.oh_dear_endpoint.always_send_fresh_results')) {
@@ -21,9 +27,19 @@ class HealthCheckJsonResultsController extends Controller
 
         $response = [
             'finishedAt' => $checkResults->finishedAt->getTimestamp(),
-            'checkResults' => $checkResults->storedCheckResults->map(fn (StoredCheckResult $line) => $line->toArray()),
+            'checkResults' => $checkResults->storedCheckResults->map(fn(StoredCheckResult $line) => $line->toArray()),
         ];
 
-        return $this->response($response, '', Response::HTTP_OK);
+        $result = $checkResults->storedCheckResults
+                ->map(fn(StoredCheckResult $line) => $line->toArray())
+                ->filter(
+                    fn(array $check) => $check['status'] === 'ok' || in_array($check['name'], self::IGNORED_CHECKS)
+                )
+                ->count() !== 0;
+
+        return $result
+            ? $this->error($response, [], Response::HTTP_SERVICE_UNAVAILABLE)
+            :
+            $this->response($response, '', Response::HTTP_OK);
     }
 }
